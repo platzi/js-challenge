d10bb182546624a1f21cb4eb8fb19bacaf72ffd2
sagarciaescobar <sa.garciaescobar@gmail.com>
2022-06-07 17:07:18 -0500

    fix: challenge

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   mode: 'development',
 };
