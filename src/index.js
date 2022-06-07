f2d4cc5d18fabb2e67a828ba499872da576982b5
Alejandro Medina <5almero5@gmail.com>
2022-06-07 13:50:56 -0500

    Solution Problem 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,31 +1,72 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
+const OFFSET = 5;
+const LIMIT = 10;
+const STORAGE_PAGE_KEY = "pageStored";
 
-const getData = api => {
+const getData = (api) => {
   fetch(api)
-    .then(response => response.json())
-    .then(response => {
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
+      let output = products.map(
+        (product) => `
+        <article class="Card">
+        <img src=${product.images[2]} alt=${product.description}/>
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>
+        `
+      );
+      let newItem = document.createElement("section");
+      newItem.classList.add("Item");
+      newItem.innerHTML = output.join(" ");
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
+
+const setLocalStorage = (key, value) => {
+  try {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.error(e);
+  }
+};
+const getLocalStorage = (key, initialValue) => {
+  try {
+    const item = window.localStorage.getItem(key);
+    return item != null ? JSON.parse(item) : initialValue;
+  } catch (e) {
+    return initialValue;
+  }
+};
 
 const loadData = () => {
-  getData(API);
-}
+  getData(
+    `${API}?offset=${getLocalStorage(STORAGE_PAGE_KEY, OFFSET)}&limit=${LIMIT}`
+  );
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    // logic...
+    const { isIntersecting } = entries[0];
+    if (isIntersecting) {
+      console.log("isIntersecting");
+      loadData();
+      setLocalStorage(
+        STORAGE_PAGE_KEY,
+        getLocalStorage(STORAGE_PAGE_KEY, OFFSET) + LIMIT
+      );
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
