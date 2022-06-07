9ee6532ece1a9ac00dc2304269046ab5e16b89a1
Milton De León <miltondeleon1996@gmail.com>
2022-06-07 10:57:22 -0600

    Primer Problema

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+localStorage.setItem("pagination", '5');
+
+let first_time = true;
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
@@ -9,6 +13,15 @@ const getData = api => {
       let products = response;
       let output = products.map(product => {
         // template
+        return `
+        <article class="Card">
+  <img src="${product.images[0]}" />
+  <h2>
+    Producto
+    <small>$ Precio</small>
+  </h2>
+</article>
+`
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -19,11 +32,20 @@ const getData = api => {
 }
 
 const loadData = () => {
-  getData(API);
+  let offset = localStorage.getItem("pagination");
+  const LIMIT = 10;
+  if (!first_time) localStorage.setItem("pagination", parseInt(offset) + LIMIT);
+  getData(API + `?offset=${offset}` + `&limit=${LIMIT}`); 
+  first_time = false;
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
