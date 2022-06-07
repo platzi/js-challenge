cc6eeca1fa95d0e4a9d116d061f8ddd48430e720
Edgar guevara <edgargn@Edgars-MacBook-Pro.local>
2022-06-07 14:49:23 -0500

    feat solved all problems asked by platzi's team for js-challenge

--- src/index.js
+++ src/index.js
@@ -1,31 +1,71 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const paginationStorage = window.localStorage;
+paginationStorage.setItem('pagination', 5);
+paginationStorage.setItem('limit', 10);
+
+const getData = async api => {
+    fetch(api)
+        .then(response => response.json())
+        .then(response => {
+
+            let products = response;
+            let limit = response.length;
+
+            let output;
+
+            if (limit < 1) {
+                intersectionObserver.disconnect();
+
+                output = (` <h3>Todos los productos Obtenidos</h3>`);
+            } else {
+                output = products.map(product => {
+                    return (`
+                    <article class="Card">
+                        <img src=${product.images} />
+                        <h2>
+                        ${product.title}
+                        <small>$ ${product.price}</small>
+                        </h2>
+                    </article>`);
+                });
+            }
+
+            let newItem = document.createElement('section');
+            newItem.classList.add('Item');
+            newItem.innerHTML = output;
+            $app.appendChild(newItem);
+
+            paginationStorage.setItem('limit', limit);
+        })
+        .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async (pagination, limit) => {
+
+    const API = `https://api.escuelajs.co/api/v1/products?offset=${pagination}&limit=${limit}`;
+
+    await getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+    // logic...
+    let pagination = Number(paginationStorage.getItem('pagination'));
+    let limit = Number(paginationStorage.getItem('limit'));
+
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+
+            if (entry.intersectionRatio >= 0.75) {
+                loadData(pagination, limit);
+                paginationStorage.setItem('pagination', pagination + limit);
+            }
+        }
+    });
+
 }, {
-  rootMargin: '0px 0px 100% 0px',
+    rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
