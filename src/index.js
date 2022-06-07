d5670f5dc93736ddc87ef2e90eb27af521798532
Werner Ovalle <wernerovalle1995@hotmail.com>
2022-06-07 15:26:08 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -2,58 +2,57 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const limit = 10;
-const totalProducts = 200;
+const maxPage = 200;
+const startOffset = 5;
+const pageLimit = 10;
 
-const getData = api => {
-  const offset = localStorage.getItem('pagination')
-  ? parseInt(localStorage.getItem('pagination')) + 10
-  : 5;
+localStorage.removeItem('pagination');
+
+const getData = async (api, offset, limit) => {
+  const url = `${api}?offset=${offset}&limit=${limit}`;    
   localStorage.setItem('pagination', offset);
-  fetch(`${api}?offset=${offset}&limit=${limit}`)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        return `<article id="${product.id}" class="Card">
-            <img src="${product.images[0]}" alt="${product.title}" />
-            <h2>
-              Producto ${product.title}
-              <small>$ ${product.price}</small>
-            </h2>
-          </article>`
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-      if (products.length < limit) {
-        let message = document.createElement('span');
-        message.innerText = 'Todos los productos Obtenidos';
-        $app.appendChild(message);
-        intersectionObserver.unobserve($observe);
-      }
-    })
-    .catch(error => console.log(error));
+
+  const response = await fetch(url);
+  const data = await response.json();  
+  const products = data.map(product => 
+    `<article class="Card Item">
+      <img src="${product.images[0]}" alt="${product.title}"/>
+      <h2> ${product.title}
+        <small>$ ${product.price}</small>
+      </h2>
+    </article>`)
+
+  let newItem = document.createElement('section');  
+  newItem.classList.add('Items');
+  newItem.innerHTML = products.join(' ');    
+  $app.appendChild(newItem);
 }
 
-const loadData = async () => {
-  await getData(API);
+const unobserve = (observe) => {
+
+  let newSection = document.createElement('section');  
+  newSection.classList.add('Item');
+  newSection.innerHTML ='<h2>Todos los productos Obtenidos</h2>';    
+  $app.appendChild(newSection);
+
+  intersectionObserver.unobserve(observe);
+}
+
+const loadData = async (offset) => {
+  await getData(API, offset, pageLimit);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-  entries.forEach(entry=>{
-    if(entry.isIntersecting)
-      loadData();
-  })
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+  if(entries[0].isIntersecting)
+  {
+    const pagination = localStorage.getItem('pagination') 
+    const offSet = pagination ? parseInt(pagination) + pageLimit : startOffset;
+
+    if(offSet - startOffset >= maxPage) 
+      return unobserve($observe);      
+
+    loadData(offSet);   
+  }  
+}, { rootMargin: '0px 0px 100% 0px', });
 
 intersectionObserver.observe($observe);
-
-window.addEventListener("beforeunload", (event) => {
-  event.preventDefault();
-  localStorage.clear();
-});
