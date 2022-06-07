d38827472e9c4006c10e50cb964df3efd123a452
Leandro Alvarez <43589282+leomaker1993@users.noreply.github.com>
2022-06-07 12:55:31 -0500

    feat(problemOne): API, localStorage, Infinite Scrolling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+let firstProduct=5;
+const nProducts=10;
+const maxProducts=200;
 
 const getData = api => {
-  fetch(api)
+  const offsetProduct=localStorage.getItem('pagination')!=null
+  ?localStorage.getItem('pagination')+nProducts
+  :firstProduct
+  localStorage.setItem('pagination',offsetProduct);
+  console.log('offset',offsetProduct)
+  fetch(`${api}?offset=${offsetProduct}&limit=${nProducts}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
+        return `<article class="Card">
+        <img src="${product.images[0]}" alt="${product.title}" />
+        <h2>
+          ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>`
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -19,11 +33,13 @@ const getData = api => {
 }
 
 const loadData = () => {
-  getData(API);
+    getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  //console.log('entries',entries)
+  if (entries[0].isIntersecting) loadData();
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
