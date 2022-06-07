cf070ecc16e5dd11e7e855f406634ad816a52c30
dylangavilan <dylan.gavilan32@gmail.com>
2022-06-07 18:12:51 -0300

    challenge finalizado

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,14 +2,14 @@
 
 Solución al reto:
 
-Nombre:
-Usuario Platzi:
-Correo Electronico:
+Nombre: Dylan Gavilan
+Usuario Platzi: dylan.gavilan32
+Correo Electronico: dylan.gavilan32@gmail.com
 
 ## Reto:
 
-- [ ] Primer problema
-- [ ] Segundo problema
-- [ ] Tercer problema
-- [ ] Cuarto Problema
-- [ ] Quinto Problema
+- [x] Primer problema
+- [x] Segundo problema
+- [x] Tercer problema
+- [x] Cuarto Problema
+- [x] Quinto Problema
--- src/index.js
+++ src/index.js
@@ -1,31 +1,64 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
+const firstPage = 5;
+const amount = 10;
+const getData = async (api) => {
+  try {
+    let response = await fetch(api);
+    response = await response.json();
+    let products = response;
+    if (response.length === 0) {
+      let title = document.createElement("h3");
+      title.innerHTML = "Todos los productos obtenidos";
+      $app.appendChild(title);
+      intersectionObserver.disconnect();
+    }
+    let output = products.map((product) => {
+      return Card(product);
+    });
+    let newItem = document.createElement("section");
+    newItem.classList.add("Item");
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const Card = (product) => {
+  return `<article class="Card">
+          <img src="${product.images[0]}" alt="${product.title}" />
+          <h2>
+            ${product.title}
+            <small>$${product.price}</small>
+          </h2>
+        </article>`;
+};
+const loadData = async () => {
+  let offset = localStorage.getItem("pagination");
+  offset = offset === NaN ? firstPage : parseInt(offset);
+  await getData(`${API}?offset=${offset}&limit=${amount}`);
+  localStorage.setItem("pagination", offset + amount);
+};
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
-
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    // logic...
+    entries.forEach((entrie) => {
+      if (entrie.isIntersecting) {
+        loadData();
+      }
+    });
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
+const init = async () => {
+  localStorage.setItem("pagination", firstPage);
+  await loadData();
+};
+init();
 
 intersectionObserver.observe($observe);
+window.onclose = localStorage.removeItem("pagination");
