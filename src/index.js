4ee40d579f2a7f0e5e87617d0d4843ccb1b1333c
Andree L <andree.lr28@gmail.com>
2022-06-07 16:37:59 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+const limit = 10;
+localStorage.clear();
+localStorage.setItem('pagination','5');
+
+const message = () => {
+  let newItem = document.createElement('message');
+  newItem.innerHTML = `<h1>Todos los productos Obtenidos</h1>`;
+  $app.appendChild(newItem);
+
+}
+
 const getData = api => {
-  fetch(api)
+  const offset = localStorage.getItem('pagination');
+
+  localStorage.setItem('pagination', parseInt(localStorage.getItem('pagination')) + 10 )
+
+  fetch(`${api}?offset=${offset - 1}&limit=${limit}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
         // template
+        return `
+        <article class="Card">
+          <img src= ${product.images[0]} />
+          <h2>
+          ${product.title}
+          <small>${product.price}</small>
+          </h2>
+        </article>
+        `
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -18,14 +42,23 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async() => {
+  await getData(API);
 }
+
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  entries.forEach(entry=>{
+    if(entry.isIntersecting) loadData();console.log(localStorage.getItem('pagination'));
+    if(localStorage.getItem('pagination') > 205 ){
+      intersectionObserver.unobserve($observe);
+      message(); 
+    }
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
+
