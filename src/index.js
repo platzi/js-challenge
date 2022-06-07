28cc0c4697ac71b6b34e0d70ef14091228128c50
GustavoMartinezFS <gustavo.martinez@isibit.travel>
2022-06-07 16:49:39 -0300

    tercer problema

--- src/index.js
+++ src/index.js
@@ -2,37 +2,38 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = url => {
-  fetch(url)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        return `
-    			<article class="Card">
-    				<img src="${product.images[0]}" alt="${product.category.name}">
-    				<h2>
-    					${product.title}
-    					<small>$ ${product.price}</small>
-    				</h2>
-    			</article>
-    		`
-      }).join('');
+localStorage.clear();
 
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async (url) => {
+  try {
+    const data = await fetch(url);
+    const products = await data.json();
+    let output = products.map(product => {
+      return `
+        <article class="Card">
+          <img src="${product.images[0]}" alt="${product.category.name}">
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>
+      `
+    }).join('');
+
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-const loadData = (offset) => {
+const loadData = async (offset) => {
   const url = `${API}?offset=${offset}&limit=10`;
-
-  getData(url);
-
   localStorage.setItem('pagination', offset);
+
+  await getData(url);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
