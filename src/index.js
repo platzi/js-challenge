3c76433ff3ab90bcc0d0699de7844fb300ee3c70
AlvaroCastillo <6666alvaro666@gmail.com>
2022-06-07 09:45:46 -0500

    ¨[ :white_check_mark: ] fix: - Se creo una estructur HTML para mostrar cada producto dentro del contenedor Items - Se creo un elemento article que contiene la clase Card en el cual se contiene la imagen, titulo y precio del articulo  -se agregaron meta tags para SEO

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
+const getData = (api) => {
   fetch(api)
-    .then(response => response.json())
-    .then(response => {
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
+      let output = products.map((product) => {
+        return Card(product);
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      let newItem = document.createElement("section");
+      newItem.classList.add("Item");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
 const loadData = () => {
   getData(API);
-}
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    // logic...
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
+
+loadData();
+
+function Card(product) {
+  console.table(product);
+
+  return `
+  <article class="Card">
+    <img src="${product.images[0]}" />
+    <h2>
+      ${product.title}
+      <small>$ ${product.price}.00</small>
+    </h2>
+  </article>`;
+}
