d65c734d6d12aafc4a1be149a7bb8e9870159c86
Armando Rivera <rivera.armando997@gmail.com>
2022-06-07 11:50:31 -0500

    challenge 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,31 +1,62 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
+const getData = (api) => {
   fetch(api)
-    .then(response => response.json())
-    .then(response => {
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
+      let productList = products.map((product) => {
+        return `
+        <article class="Card">
+          <img width=640 height=480 src=${product.images[0]} />
+          <h2>
+            ${product.title}
+            <small>${product.price}</small>
+          </h2>
+        </article>
+        `;
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      renderProducts(productList);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
+
+const renderProducts = (productList) => {
+  let newItem = document.createElement("section");
+  newItem.classList.add("Item");
+  newItem.innerHTML = productList;
+  $app.appendChild(newItem);
+};
 
 const loadData = () => {
   getData(API);
-}
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      getData(getURL(getOffset(), 10));
+      setOffset(getOffset() + 5);
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
+
+const getOffset = () => {
+  const offset = localStorage.getItem("offset") || "5";
+  return Number(offset);
+};
+
+const setOffset = (offset) => {
+  localStorage.setItem("offset", offset);
+};
+
+const getURL = (offset, limit) => {
+  return `${API}?offset=${offset}&limit=${limit}`;
+};
