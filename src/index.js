5aab0cd729601775c556d580bd75d135bff535c5
GeometricCard <polargx@gmail.com>
2022-06-07 13:52:49 -0500

    Solucion Reto

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const LIMIT = 10;
+const FIRST_ITEM = 5;
+const MAX_RESULT = 200;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+localStorage.clear();
+localStorage.setItem("pagination", FIRST_ITEM);
+
+const getData = async api => {
+  try{
+    let response = await fetch(api);
+    let products = await response.json();
+  
+    let output = products.map(product => {
+        return `
+        <article class="Card">
+        <img src="${product.images}"/>
+        <h2>
+          Producto
+          <small>$ Precio ${product.price}</small>
+        </h2>
+        </article>`
+    });
+    let newItem = document.createElement('section');
+    newItem.classList.add('Item');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+  }catch(e){
+    console.log(e);
+  }
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  let offset = parseInt(localStorage.getItem("pagination"));
+  if(offset <= MAX_RESULT){
+    localStorage.setItem("pagination", offset + LIMIT);
+    await getData(API + '?offset=' + offset + '&limit=' + LIMIT);
+  }else{
+      let newItem = document.createElement('section');
+      newItem.innerHTML = `<p>Todos los productos Obtenidos</p>`;
+      $app.appendChild(newItem);
+      intersectionObserver.unobserve($observe);
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      loadData();
+    }
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
