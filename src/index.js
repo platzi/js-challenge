17944e1c037b9f22715be685e61b7111c842c228
Ivan Guerrero <ivan.guerrero@businet-web.com>
2022-06-07 13:51:21 -0500

    Solucion del Platzi Challenge - Ivan Guerrero

--- src/index.js
+++ src/index.js
@@ -1,31 +1,86 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const limitProducts = 200;
+const incrementOffset = 10;
+const defaultOffset = 5;
+window.onbeforeunload = closeIt;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async (api) => {
+  const responseProducts = await fetch(api);
+  try {
+    const response = await responseProducts.json();
+    let products = response;
+    let productList = products.map(createCard);
+    renderProducts(productList);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createCard = (product) => {
+  return `
+    <article class='Card'>
+      <img width=640 height=480 src=${product.images[0]} />
+      <h2>
+        ${product.title}
+        <small>${product.price}</small>
+      </h2>
+    </article>
+  `;
+};
+
+const renderProducts = (productList) => {
+  let newItem = document.createElement('section');
+  newItem.classList.add('Item');
+  newItem.innerHTML = productList.join('');
+  $app.appendChild(newItem);
+};
+
+const resetStorage = () => {
+  localStorage.removeItem('offset');
+};
+
+function closeIt() {
+  resetStorage();
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  await getData(API);
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    if (getOffset() > limitProducts) {
+      showMessage('Todos los productos obtenidos');
+      intersectionObserver.disconnect();
+      return;
+    }
+    if (entries[0].isIntersecting) {
+      getData(getURL(getOffset(), incrementOffset));
+      setOffset(getOffset() + incrementOffset);
+    }
+  },
+  {
+    rootMargin: '0px 0px 100% 0px',
+  }
+);
 
 intersectionObserver.observe($observe);
+
+const showMessage = (message) => {
+  window.alert(message);
+};
+
+const getOffset = () => {
+  const offset = localStorage.getItem('offset') || defaultOffset;
+  return Number(offset);
+};
+
+const setOffset = (offset) => {
+  localStorage.setItem('offset', offset);
+};
+
+const getURL = (offset, limit) => {
+  return `${API}?offset=${offset}&limit=${limit}`;
+};
