e3577d356b18bdd4f2856c73eab3b819fdecb021
Fernando Gómez <ferjgmorales25@gmail.com>
2022-06-07 12:45:32 -0500

    fixing localstorage misunderstood

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 const ITEMS_PER_PAGE = 10;
-let OFFSET = 5;
-localStorage.setItem('pagination', OFFSET);
+localStorage.clear('pagination');
 
 // Las instrucciones decian que debia refactorizar la funcion load data
 // pero no tiene mucho sentido ya que esa funcion solo llamaba a getData
 // y no hace nada mas. Asi que refactorice getData tambien
-const getData = async api => {
+const getData = async (api, offset) => {
   try {
     const rawResponse = await fetch(
-      `${api}?offset=${OFFSET}&limit=${ITEMS_PER_PAGE}`
+      `${api}?offset=${offset}&limit=${ITEMS_PER_PAGE}`
     );
     const products = await rawResponse.json();
     let output = products.map(
@@ -25,27 +24,18 @@ const getData = async api => {
           </article>
           `
     );
-    OFFSET += ITEMS_PER_PAGE;
     let newItem = document.createElement('section');
     newItem.classList.add('Items');
     newItem.innerHTML = output.join('');
     $app.appendChild(newItem);
+    localStorage.setItem('pagination', offset);
   } catch (error) {
     throw new Error('[getData] - Error al cargar los datos');
   }
 };
 
-const loadData = async () => {
-  try {
-    await getData(API);
-  } catch (error) {
-    console.error(error);
-    let newItem = document.createElement('div');
-    newItem.classList.add('error-msg');
-    newItem.innerHTML = 'Algo salio mal';
-    $app.appendChild(newItem);
-  }
-  if (OFFSET >= 200) {
+const loadData = async offset => {
+  if (offset >= 200) {
     $observe.remove();
     let newItem = document.createElement('div');
     newItem.classList.add('msg');
@@ -53,13 +43,25 @@ const loadData = async () => {
     newItem.style.padding = '2rem 0';
     newItem.innerHTML = 'Todos los productos Obtenidos';
     $app.appendChild(newItem);
+    return;
+  }
+
+  try {
+    await getData(API, offset);
+  } catch (error) {
+    console.error(error);
+    let newItem = document.createElement('div');
+    newItem.classList.add('error-msg');
+    newItem.innerHTML = 'Algo salio mal';
+    $app.appendChild(newItem);
   }
 };
 
 const intersectionObserver = new IntersectionObserver(
   entries => {
     if (entries[0].isIntersecting) {
-      loadData();
+      const offset = localStorage.getItem('pagination');
+      loadData(offset || 5);
     }
   },
   {
