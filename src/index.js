96faab05677c40c7f4b588e2dd8dd8409e07f294
Carlos Meneses <carlosamenesesc@gmail.com>
2022-06-07 13:21:58 -0500

    [ADD] first version

--- src/index.js
+++ src/index.js
@@ -2,30 +2,61 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+let offset = 4,
+  limit = 10,
+  loading = false;
+
 const getData = api => {
-  fetch(api)
+  loading = true;
+  if (offset < 200) {
+    fetch(`${api}?offset=${offset}&limit=${limit}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
+        return `
+          <article class="Card">
+            <img src="${product.category.image}" />
+            <h2>
+              ${product.title}
+              <small>$ ${product.price}</small>
+            </h2>
+          </article>
+        `;
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      localStorage.setItem('pagination', offset);
+      console.log(offset);
+      offset += limit;
+      setTimeout(() => {
+        loading = false;
+      }, 1000);
     })
     .catch(error => console.log(error));
+  } else {
+    alert('No hay mas productos');
+    observer.disconnect();
+  }
+
 }
 
-const loadData = () => {
-  getData(API);
+async function loadData() {
+  if (!loading) {
+    await getData(API);
+  }
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+window.close = () => {
+  Storage.clear();
+}
 
-intersectionObserver.observe($observe);
+const intersectOptions = {
+  threshold: 1,
+};
+
+let observer = new IntersectionObserver(loadData, intersectOptions)
+
+observer.observe($observe);
