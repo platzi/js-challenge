58c71ea4113c7b5f53acb72809597475c5668f6f
Eduardo P. Rivero <jrperedo@gmail.com>
2022-06-07 15:21:22 -0500

    feat: js challenge implementation

--- src/index.js
+++ src/index.js
@@ -1,31 +1,65 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+window.localStorage.clear();
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products?";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+function showAlert() {
+  const $el = document.createElement("div");
+  $el.classList.add("alert");
+  $el.innerHTML = '<p class="alert-text">Todos los productos Obtenidos</p>';
+  $app.appendChild($el);
 }
 
-const loadData = () => {
-  getData(API);
-}
+const getData = async (api, { offset, limit }) => {
+  const url = new URLSearchParams();
+  url.append("offset", offset);
+  url.append("limit", limit);
+  const response = await fetch(`${api}${url}`);
+  const products = await response.json();
+  let output = products.reduce((html, product) => {
+    const image = Array.isArray(product.images)
+      ? product.images[0]
+      : product.category.image;
+    html += `<article class="Card">
+    <img src=${image} />
+    <h2>
+      ${product.title}
+      <small>$ ${product.price}</small>
+    </h2>
+  </article>`;
+    return html;
+  }, "");
+  let newItem = document.createElement("section");
+  newItem.classList.add("Items");
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const loadData = async (offset) => {
+  await getData(API, { offset, limit: 10 });
+  window.localStorage.setItem("pagination", offset);
+};
+
+const intersectionObserver = new IntersectionObserver(
+  async (entries) => {
+    if (entries[0].isIntersecting) {
+      const pagination = Number(window.localStorage.getItem("pagination"));
+      if (pagination && !Number.isNaN(pagination)) {
+        const currentPagination = pagination + 5;
+        if (currentPagination < 200) {
+          await loadData(currentPagination);
+        } else {
+          intersectionObserver.disconnect();
+          showAlert();
+        }
+      } else {
+        await loadData(5);
+      }
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
