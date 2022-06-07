f64d0ff1d3c4b4e160db41251f44045c38b6f627
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-06-07 13:45:49 -0300

    Problemas 1, 2 y 4 resueltos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+const FIRSTITEM = 185;
+const LOADAMOUNT = 10;
+const MAXITEMS = 200;
+localStorage.setItem("Pagination", FIRSTITEM-1);
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
@@ -17,13 +22,54 @@ const getData = api => {
     })
     .catch(error => console.log(error));
 }
+const getDataPagination = (api, limit) => {
+  let pagination = localStorage.getItem("Pagination");
+  let params = `?offset=${pagination}&limit=${limit}`;
+  fetch(api+params)
+    .then(response => response.json())
+    .then(response => {
+      let products = response;
+      let output = products.map(product => {
+        if (product.id <=200) {
+            return(`
+            <article class="Card">
+              <img src="${product.images[0]}" />
+              <h2>
+                ${product.title}
+                <small>$ ${product.price}</small>
+              </h2>
+            </article>`)
+        } else{
+          return("Todos los productos Obtenidos")
+        }
+        });
+      let newItem = document.createElement('section');
+      newItem.classList.add('Item');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+    })
+    .catch(error => console.log(error));
+};
 
 const loadData = () => {
-  getData(API);
+  getDataPagination(API, LOADAMOUNT);
+  pagination = parseInt(localStorage.getItem("Pagination"), 10);
+  console.log(localStorage.getItem("Pagination"));
+  if(pagination + LOADAMOUNT <= MAXITEMS) {
+    localStorage.setItem("Pagination", pagination + LOADAMOUNT);
+  } else{
+    intersectionObserver.unobserve($observe);
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  })
+
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
