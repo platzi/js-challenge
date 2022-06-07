97a83a78664a702d2ff286c1e2c463e2e2efec02
Khevin_Efraín <khevinstools@gmail.com>
2022-06-07 15:25:53 -0500

    Challenge -Solution

---
+++ public/index.js
@@ -0,0 +1,58 @@
+const $app = document.getElementById('app');
+const $observe = document.getElementById('observe');
+const API = `https://api.escuelajs.co/api/v1/products`;
+
+localStorage.clear()
+
+const getData = (api, limit, offset) => {
+  localStorage.setItem('pagination', offset)
+  const urlApiProducts = `${api}?offset=${offset}&limit=${limit}`;
+  fetch(urlApiProducts)
+  .then(response => response.json())
+  .then(response => {
+    let products = response;
+    let output = products.map(product => 
+      `
+        <article class="Card">
+          <img src="${product.images[0]}" alt="${product.title}"/>
+          <h2>
+            ${product.title}
+            <small> $ ${product.price}</small>
+          </h2>
+        </article>
+      `
+    );
+      let newItem = document.createElement('section');
+      newItem.classList.add('Item');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+
+      if(products.length < limit) {
+        let message = document.createElement('p');
+        message.innerHTML = 'Todos los productos Obtenidos';
+        $app.appendChild(message);
+        intersectionObserver.unobserve($observe);
+      }
+
+    })
+    .catch(error => console.log(error));
+}
+
+const loadData = async (offset) => {
+  const limit = 10;
+  await getData(API, limit, offset);
+}
+
+const intersectionObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry =>{
+    const { isIntersecting, target } = entry;
+    if(isIntersecting) {
+      const newOffset = !localStorage.getItem('pagination') ? 5 : parseInt(localStorage.getItem('pagination')) + 10;
+      loadData(newOffset)  
+    }
+  })
+}, {
+  rootMargin: '0px 0px 100% 0px',
+});
+
+intersectionObserver.observe($observe);
