e2933052609431ae827446fc1370c49675e75289
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2022-06-07 10:49:23 -0500

    Nombre: Esteban Danilo Diaz Riascos Usuario Platzi: stban94diazz Correo Electronico: stban94diaz@gmail.com

--- src/index.js
+++ src/index.js
@@ -2,28 +2,48 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
+localStorage.setItem('offset', 4);
+
+const getData = async (api) => {
+  return fetch(`${api}?offset=${localStorage.getItem('offset')}&limit=10`)
     .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      localStorage.setItem('offset', parseInt(localStorage.getItem('offset')) + 10);
+      return response.json();
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      localStorage.setItem('offset', parseInt(localStorage.getItem('offset')) + 10);
+      console.log(error);
+    });
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  const products = await getData(API);
+  let output = products.map(product => {
+    return `
+      <article class="Card">
+        <img src="${product.images[0]}" />
+        <h2>
+          ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>
+    `;
+  });
+  let newItem = document.createElement('section');
+  newItem.classList.add('Item');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  loadData();
+  if (localStorage.getItem('offset') >= 200) {
+    intersectionObserver.unobserve($observe);
+    let newItem = document.createElement('section');
+    newItem.innerHTML = `<h1>Todos los productos Obtenidos</h1>`;
+    $app.appendChild(newItem);
+  }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
