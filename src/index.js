7cf1717e1300741e81d4666b67db2ce7b868587e
Francisco Antonio Cruz Julio <francisco@Franciscos-MacBook-Pro.local>
2022-06-07 12:32:31 -0500

    fix(PlatziStore js challenge): Resolved points #1, 2, 3 and 4 of the challenge

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById('app')
+const $observe = document.getElementById('observe')
+const API = 'https://api.escuelajs.co/api/v1/products'
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async (api, limit = 10) => 
+{
+
+  const offset = localStorage.getItem('pagination') ? parseInt(localStorage.getItem('pagination')) + 10 : 5
+
+  localStorage.setItem('pagination', offset)
+
+  const url = `${api}?offset=${offset}&limit=${limit}`
+  const data = await fetch(url)
+  const products = await data.json()
+
+  let output = products.map(product => 
+		
+	`<article id="${product.id}" class="Card">
+		<img src="${product.images[0]}" alt="${product.title}" />
+		<h2>
+			Producto ${product.title}
+			<small>$ ${product.price}</small>
+		</h2>
+		</article>`
+	)
+
+  let newItem = document.createElement('section')
+  newItem.classList.add('Items')
+  newItem.innerHTML = output
+  $app.appendChild(newItem)
+
+  if (products.length < limit) 
+  {
+    let message = document.createElement('span')
+	message.innerText = 'Todos los productos Obtenidos'
+	$app.appendChild(message)
+	intersectionObserver.unobserve($observe)
+  }
 }
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = () => getData(API).catch( error => console.error(error))
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(entries => 
+{
+  entries.forEach(entry => { if (entry.isIntersecting) loadData() })
+}, 
+{ rootMargin: '0px 0px 100% 0px'})
 
-intersectionObserver.observe($observe);
+intersectionObserver.observe($observe)
+
+window.addEventListener("beforeunload", (event) => 
+{
+  event.preventDefault()
+  localStorage.clear()
+})
