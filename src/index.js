054b5efa932e20c4cd6fb72ae17b58df1f833630
Maria Paula Rodriguez Escobar <mariapaularoes@gmail.com>
2022-06-07 18:25:24 -0500

    Corrige el índice de inicio a 5
    - Actualiza la forma en la que se gestiona el localStorage
    - Agrega un div para encerrar un grupo de Items

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
-const INIT_INDEX = 4;
+const INIT_INDEX = 5;
 const LIMIT = 10;
 
 localStorage.clear();
 
 const getPagination = () => {
   const pagination = parseInt(localStorage.getItem("pagination"));
-  return Number.isInteger(pagination) ? pagination : INIT_INDEX;
+  return Number.isInteger(pagination) ? pagination+LIMIT : INIT_INDEX;
 };
 
 const getData = async (api, limit, offset) => {
@@ -24,8 +24,7 @@ const getData = async (api, limit, offset) => {
       // Stop observing
       intersectionObserver.unobserve($observe);
     } else {
-      localStorage.setItem('pagination', getPagination()+LIMIT);
-      output = products.map(product => {
+      output = "<div class='Items'>" + products.map(product => {
         return `<article class="Card">
           <img src="${product.images[0]}" onerror="console.log('The image could not be loaded.');"/>
           <h2>
@@ -33,8 +32,8 @@ const getData = async (api, limit, offset) => {
             <small>$ ${product.price}</small>
           </h2>
         </article>`;
-      }).join("");
-      newItem.classList.add('Items');
+      }).join("") + "<div>";
+      newItem.classList.add('Item');
     }
     newItem.innerHTML = output;
     $app.appendChild(newItem);
@@ -45,7 +44,7 @@ const getData = async (api, limit, offset) => {
 
 const loadData = async () => {
   localStorage.setItem('pagination', getPagination());
-  await getData(API, LIMIT, getPagination());
+  getData(API, LIMIT, getPagination());
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
