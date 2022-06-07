081ab5e72f01214a6d255b10f441edaa25f14c8d
irving <irvoma@gmail.com>
2022-06-07 16:10:50 -0500

    feat: solution to the fourth problem

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products?limit=10';
+const API = 'https://api.escuelajs.co/api/v1/products';
+const TOTAL_PRODUCTS = 200;
 
-const getData = async (api, offset) => {
-  api += `&offset=${offset}`;
+const getData = async (api, offset, limit) => {
+  api += `?offset=${offset}&limit=${limit}`;
   try {
     const response = await fetch(api);
     const responseJson = await response.json();
@@ -35,19 +36,33 @@ const getPage = () => {
 
 const setPage = page => window.localStorage.setItem('pagination', page);
 
-const loadData = async () => {
-  let page = getPage();
-  let data = await getData(API, page);
+const loadData = async (page, limit) => {
+  let data = await getData(API, page, limit);
   let items = $app.getElementsByClassName('Items')[0];
   data.forEach(item => items.appendChild(item));
-  setPage(page + 10);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
   let entry = entries[0];
   if (!entry.isIntersecting) return;
-  loadData();
+
+  let page = getPage();
+
+  if (page + 10 > TOTAL_PRODUCTS) {
+    loadData(page, TOTAL_PRODUCTS - page);
+
+    let message = document.createElement('h2');
+    message.style.textAlign = 'center';
+    message.innerHTML = 'Todos los productos Obtenidos';
+    $app.appendChild(message);
+
+    intersectionObserver.unobserve($observe);
+    return;
+  }
+  
+  loadData(page, 10);
+  setPage(page + 10);
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
