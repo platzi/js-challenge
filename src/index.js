774237132c15ee4b989ab24815b75afdb0a5fd20
sagarciaescobar <sa.garciaescobar@gmail.com>
2022-06-07 17:41:39 -0500

    correccion

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const template = require('./templates');
+const api = require('./api/api');
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+localStorage.removeItem('pagination');
+localStorage.setItem('pagination', '0');
 
-const loadData = () => {
-  getData(API);
-}
+let newItem = document.createElement('section');
+newItem.classList.add('Item');
+newItem.appendChild(document.createElement('ul'));
+$app.appendChild(newItem);
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const renderData = async page => {
+  const list = document.getElementsByClassName('Item')[0].children[0];
+  const products = await api.getProducts(page);
+  template.items(list, products);
+};
+
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting === true) {
+      let page = parseInt(localStorage.getItem('pagination')) + 1;
+      localStorage.setItem('pagination', page);
+      renderData(page);
+      console.log(entries);
+    }
+  },
+  {
+    threshold: 1.0,
+    rootMargin: '0px 0px 100% 0px',
+  }
+);
 
 intersectionObserver.observe($observe);
+
+const mutation = new MutationObserver(() => {
+  let page = localStorage.getItem('pagination');
+  if (page === '20') {
+    intersectionObserver.disconnect();
+    $app.innerHTML += 'Todos los productos Obtenidos';
+  }
+});
+
+const list = document.getElementsByClassName('Item')[0];
+
+mutation.observe(list, { childList: true, subtree: true });
