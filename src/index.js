3b23f3b200070de83fe1d5b7204930fa88e276bd
Maria Paula Rodriguez Escobar <mrodriguez1@gannett.com>
2022-06-07 11:30:00 -0500

    Tercer Problema
    - Mostrar los primeros 10 productos.
    - Eliminar el localStorage.
    - Actualiza la función loadData() a Async/Await.

--- src/index.js
+++ src/index.js
@@ -4,43 +4,40 @@ const API = 'https://api.escuelajs.co/api/v1/products';
 const INIT_INDEX = 4;
 const LIMIT = 10;
 
-const getPagination = () => {
-  const pagination = parseInt(localStorage.getItem("pagination"));
-  return Number.isInteger(pagination) ? pagination : INIT_INDEX;
-};
+let pagination = INIT_INDEX;
 
-const getData = (api, limit, offset) => {
-  fetch(`${api}?limit=${limit}&offset=${offset}`)
-    .then(response => response.json())
-    .then(response => {
-      localStorage.setItem('pagination', getPagination()+LIMIT);
-      let products = response;
-      let output = products.map(product => {
-        return `<article class="Card">
-          <img src="${product.images[0]}" />
-          <h2>
-          ${product.title}
-            <small>$ ${product.price}</small>
-          </h2>
-        </article>`;
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = `<main class="Main">${output.join("")}</main>`; 
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const getData = async (api, limit, offset) => {
+  try {
+    let response = await fetch(`${api}?limit=${limit}&offset=${offset}`);
+    response = await response.json();
+    pagination+=LIMIT;
+    let products = response;
+    let output = products.map(product => {
+      return `<article class="Card">
+        <img src="${product.images[0]}" />
+        <h2>
+        ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>`;
+    });
+    let newItem = document.createElement('section');
+    newItem.classList.add('Item');
+    newItem.innerHTML = `<main class="Main">${output.join("")}</main>`;
+    $app.appendChild(newItem);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const loadData = (limit, offset) => {
-  getData(API, limit, offset);
+const loadData = async (limit, offset) => {
+  await getData(API, limit, offset);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   entries.map(entrie => {
     if (entrie.isIntersecting) {
-      localStorage.setItem('pagination', getPagination());
-      loadData(LIMIT, getPagination());
+      loadData(LIMIT, pagination);
     }
   });
 }, {
