6fb6fbd8021de2a64fa55b7debb6c3dc12d7f63e
Frcomparan <frcomparan@gmail.com>
2022-06-07 13:39:54 -0500

    Primer problema finalizado

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const STEP = 10;
+let pagination = 5;
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
+      let output = products.filter(product => (product.id >= pagination && product.id < (STEP + pagination)));
+      pagination += STEP;
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
@@ -29,3 +30,13 @@ const intersectionObserver = new IntersectionObserver(entries => {
 });
 
 intersectionObserver.observe($observe);
+
+loadData();
+
+const infinitScroll = () => {
+  if (document.body.scrollHeight - window.innerHeight === window.scrollY) {
+    console.log('estoy en el final del scroll');
+   }
+ }
+ 
+ window.addEventListener('scroll', infinitScroll);
