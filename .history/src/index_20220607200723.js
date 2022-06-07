229132fb20dd0fdbf26956f0f12e7f18daf318de
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-06-07 20:17:29 +0200

    commit 01

---
+++ .history/src/index_20220607200723.js
@@ -0,0 +1,31 @@
+const $app = document.getElementById('app');
+const $observe = document.getElementById('observe');
+const API = 'https://api.escuelajs.co/api/v1/products';
+
+const getData = api => {
+  fetch(api)
+    .then(response => response.json())
+    .then(response => {
+      let products = response;
+      let output = products.map(product => {
+        // template
+      });
+      let newItem = document.createElement('section');
+      newItem.classList.add('Item');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+    })
+    .catch(error => console.log(error));
+}
+
+const loadData = () => {
+  getData(API);
+}
+
+const intersectionObserver = new IntersectionObserver(entries => {
+  // logic...
+}, {
+  rootMargin: '0px 0px 100% 0px',
+});
+
+intersectionObserver.observe($observe);
