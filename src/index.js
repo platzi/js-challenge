0ed0053cd40003d4efabf11ce343053ea72f5e1f
mgoscar <mgoscar@gmail.com>
2022-06-07 18:14:21 -0500

    Correcciones realizadas

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const getData = api => {
-  fetch(api)
+  const offset = localStorage.getItem('pagination') //Problema 1.2
+  ? parseInt(localStorage.getItem('pagination')) + 10 //Si hay un valor, trae los siguientes 10
+  : 5; //Si no, empieza con el producto 5
+  localStorage.setItem('pagination', offset); //Problema 1.3
+  fetch(`${api}?offset=${offset}&limit=10`)
     .then(response => response.json())
     .then(response => {
       let products = response;
       //console.log('PRODUCTOS=>'+JSON.stringify(products));
       let output = products.map(product => { //Problema 2.2
 			  return `<article class="Card"> 
-                  <img src="${product.images[0]}" alt="${product.category.name}">
+                  <img src="${product.images[0]}" alt="${product.title}">
 				          <h2>${product.title}
 					          <small>$ ${product.price}</small>
 				          </h2>
@@ -21,31 +25,25 @@ const getData = api => {
       //console.log('OUTPUT=>'+output);
       newItem.innerHTML = output;
       $app.appendChild(newItem);
-      
-      if (products.length < 10) {
-        const message = document.createElement('p');
-        message.innerHTML = 'Todos los productos Obtenidos'; //Problema 4.1 
-        $app.appendChild(message);
-        intersectionObserver.unobserve($observe); //Problema 4.2
-      }
     })
     .catch(error => console.log(error));
 } 
 
-const loadData =  async () => {   //Problema 3.3
-  const offset = localStorage.getItem('pagination') //Problema 1.2
-  ? parseInt(localStorage.getItem('pagination')) + 10 //Si hay un valor, trae los siguientes 10
-  : 5; //Si no, empieza con el producto 5
-  localStorage.setItem('pagination', offset); //Problema 1.3
-  await getData(`${API}?offset=${offset}&limit=10`);
+const loadData =  async () => {   //Problema 3.3  
+  await getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
-  entries.forEach(entry=>{ //Problema 1.4
-    if(entry.isIntersecting)
-      loadData();
-  })
+  console.log('PAGINATION=>'+localStorage.getItem('pagination')+' ISINTERSECTING=>'+entries[0].isIntersecting);
+  if (entries[0].isIntersecting) {
+    loadData();
+  } else if(Number(localStorage.getItem('pagination'))>=200){
+    const message = document.createElement('p');
+    message.innerHTML = 'Todos los productos Obtenidos'; //Problema 4.1 
+    $app.appendChild(message);
+    intersectionObserver.unobserve($observe); //Problema 4.2
+  }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
