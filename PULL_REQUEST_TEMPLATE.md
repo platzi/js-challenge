933c5c28f967eae5516348f8a25a74374790eee4
ricky <ricky@brein.pe>
2022-06-07 13:25:10 -0500

    feat:Finish challenges

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,14 +2,14 @@
 
 Solución al reto:
 
-Nombre:
-Usuario Platzi:
-Correo Electronico:
+Nombre: Ricky Junior Pareja Medina
+Usuario Platzi: rickyparejamedina
+Correo Electronico: rickyparejamedina@gmail.com
 
 ## Reto:
 
-- [ ] Primer problema
-- [ ] Segundo problema
-- [ ] Tercer problema
-- [ ] Cuarto Problema
-- [ ] Quinto Problema
+-   [x] Primer problema
+-   [x] Segundo problema
+-   [x] Tercer problema
+-   [x] Cuarto Problema
+-   [x] Quinto Problema
--- src/index.js
+++ src/index.js
@@ -1,31 +1,64 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const initalPagination = 5
+const limit = 10
 
-const getData = api => {
-  fetch(api)
+const getData = async (api) => {
+
+  let pagination = Number(localStorage.getItem('pagination'))
+  if (!pagination) {
+    localStorage.setItem('pagination', initalPagination)
+    pagination = initalPagination
+  } else {
+    pagination = pagination + limit
+    localStorage.setItem('pagination', pagination)
+  }
+
+  fetch(`${api}?offset=${pagination-1}&limit=${limit}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
+
       let output = products.map(product => {
-        // template
+        return `<article class="Card">
+          <img src="${product.images[0]}" />
+          <h2>
+          ${product.id}-${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>`
+
       });
+      if(products.length == 0){
+        alert('Todos los productos Obtenidos')
+        intersectionObserver.disconnect()
+      }
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  await getData(API)
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      loadData()
+    }
+  })
+
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
+
+window.onbeforeunload = function () {
+  localStorage.clear()
+}
