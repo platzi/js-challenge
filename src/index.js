b0d1ccff7948e3270b728217976ab45f32173810
Emanuel Sandries <76988982+ESandries@users.noreply.github.com>
2022-06-07 23:39:12 +0000

    version 1

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products?offset=5&limit=10";
 
-const getData = api => {
+const getData = (api) => {
   fetch(api)
-    .then(response => response.json())
-    .then(response => {
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
+      let output = products.map((product) => {
+        return `<article class="Card">
+          <img src="${product.category.image}" />
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+          <p>${product.id}</p>
+        </article>`
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items");
+      newItem.innerHTML = output.toString().split(",").join("");
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
 const loadData = () => {
   getData(API);
-}
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    // logic...
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
+
+loadData();
