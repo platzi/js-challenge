8319152cf2211715b396ee72ce8605e56ae48010
Panchop10 <folivares@arenasoft.cl>
2022-06-08 00:09:44 +0100

    primer problema

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+// Check pagination
+const pagination = parseInt(localStorage.getItem("pagination"));
+
+// API settings
+const limit = 10;
+let offset = pagination ? limit + pagination : 5;
+
+// Store pagination 
+localStorage.setItem("pagination", offset)
+
+localStorage.getItem("lastname");
 const getData = api => {
-  fetch(api)
+  fetch(`${api}/?offset=${offset}&limit=${limit}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
         // template
+        return `<article>${product.id}</article>`;
       });
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
@@ -23,7 +35,7 @@ const loadData = () => {
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  loadData();
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
