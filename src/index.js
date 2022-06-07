db00e28d946e3d7fee6fa86de5465fe323b57d6b
Maria Paula Rodriguez Escobar <mrodriguez1@gannett.com>
2022-06-07 11:14:53 -0500

    Primer problema
    - Analiza la API: fakeapi.platzi.com
    - Implementa la API de productos iniciando en el producto 5 y obteniendo los siguientes 10 productos.
    - Guarda en localStorage la posición inicial ("pagination") y actualízala en cada petición nueva para traer los siguientes productos.
    - Crear la lógica para implementar un scroll infinito con Intersection Observer.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const INIT_INDEX = 4;
+const LIMIT = 10;
 
-const getData = api => {
-  fetch(api)
+const getPagination = () => {
+  const pagination = parseInt(localStorage.getItem("pagination"));
+  return Number.isInteger(pagination) ? pagination : INIT_INDEX;
+};
+
+const getData = (api, limit, offset) => {
+  fetch(`${api}?limit=${limit}&offset=${offset}`)
     .then(response => response.json())
     .then(response => {
+      localStorage.setItem('pagination', getPagination()+LIMIT);
       let products = response;
       let output = products.map(product => {
         // template
@@ -18,12 +26,17 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = (limit, offset) => {
+  getData(API, limit, offset);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.map(entrie => {
+    if (entrie.isIntersecting) {
+      localStorage.setItem('pagination', getPagination());
+      loadData(LIMIT, getPagination());
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
