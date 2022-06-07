f46ce9391a9c8deb8f1fcced3ff2fb6ad8e52811
Alberto Silva <silvacazaresluis@outlook.com>
2022-06-07 12:01:34 -0500

    Solved challenges

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,14 +2,14 @@
 
 Solución al reto:
 
-Nombre:
-Usuario Platzi:
-Correo Electronico:
-
+Nombre: Alberto Silva 
+Usuario Platzi: albertosilva
+Correo Electronico: silvacazaresluis@outlook.com
+ 
 ## Reto:
 
-- [ ] Primer problema
-- [ ] Segundo problema
-- [ ] Tercer problema
-- [ ] Cuarto Problema
-- [ ] Quinto Problema
+- [x] Primer problema
+- [x] Segundo problema
+- [x] Tercer problema
+- [x] Cuarto Problema
+- [x] Quinto Problema
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const speedLoad = 1000;
 
-const getData = api => {
+localStorage.clear();
+localStorage.setItem('pagination',5);
+const getData = (api) => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
+        return `<article class="Card">
+          <img src="${product.images[0]}" alt="${product.title}"/>
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>`
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -18,12 +27,30 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+
+const loadData = async (offset) => {
+  let pagination =`?offset=${offset}&limit=10`;
+  i = 0;
+  setTimeout(() => {
+    getData(API+pagination);
+  },speedLoad)
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // let offset = parseInt(localStorage.getItem('pagination'))?parseInt(localStorage.getItem('pagination')):5;
+      let offset = parseInt(localStorage.getItem('pagination'));
+      if(offset>=200){
+        $observe.innerHTML="<h2>Todos los productos obtenitdos</h2>";
+        intersectionObserver.disconnect();
+        return;
+      }
+      (async () => await loadData(offset))();
+      localStorage.setItem('pagination',offset+10);
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
