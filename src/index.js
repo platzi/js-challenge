9641a662e8e53a4763295a28f577c3a883a68e58
Jeymarson <jgarcia@dlocal.com>
2022-06-07 11:08:24 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,31 +1,78 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+const initPosition = 0;
+const limit = 10;
+
+const getData = (api, offset, limit) => {
+  fetch(`${api}?offset=${offset}&limit=${limit}`)
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      if (products.length) {
+        let output = products.map((product) => {
+          return `
+          <article class="Card">
+            <img src="${product.category.image}" />
+            <h2>
+              ${product.title}
+              <small>$ ${product.price}</small>
+            </h2>
+          </article>
+          `;
+        });
+        let newItem = document.createElement("section");
+        newItem.classList.add("Item");
+        newItem.innerHTML = output;
+        $app.appendChild(newItem);
+      }
+
+      if (products.length < limit) {
+        let output = `
+        <h2>
+          Todos los productos Obtenidos
+        </h2>
+      `;
+
+        let newItem = document.createElement("section");
+        newItem.classList.add("Item");
+        newItem.innerHTML = output;
+        $app.appendChild(newItem);
+        intersectionObserver.unobserve($observe);
+      }
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
 const loadData = () => {
-  getData(API);
-}
+  let positionString = localStorage.getItem("pagination");
+  let position = initPosition;
+  if (!isNaN(positionString)) {
+    position = Number(positionString);
+  }
+  localStorage.setItem("pagination", position + limit);
+  getData(API, position, limit);
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const init = () => {
+  localStorage.setItem("pagination", initPosition + limit);
+  getData(API, initPosition, limit);
+};
+
+document.onload = init();
+
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadData();
+      }
+    });
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
