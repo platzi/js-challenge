89498f73bcb2fb149ff4019515631564bb8035d3
AlvaroCastillo <6666alvaro666@gmail.com>
2022-06-07 11:25:48 -0500

    ¨[ :white_check_mark: ] fix: -Se implementa la api para pedir los primero 10 articulos partiendo del numero 5 - Se guarda el valor de la pagina en el localStorage y se actualiza el valor de la pagina solicitada - Se implementa la funcion de infinity scroll

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ const $app = document.getElementById("app");
 const $observe = document.getElementById("observe");
 const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = (api) => {
-  fetch(api)
+const initalLimit = 4;
+const Limit = 10;
+var Page = 0;
+
+const createUrl = (limit = Limit, offset = initalLimit) => {
+  return `${API}?offset=${offset}&limit=${limit}`;
+};
+
+const addPage = () => {
+  savePage(Page);
+  const page = Page * Limit + initalLimit;
+  Page++;
+  return page;
+};
+
+const savePage = (page) => {
+  localStorage.setItem("page", page);
+};
+
+const getData = (offset) => {
+  fetch(createUrl(Limit, offset))
     .then((response) => response.json())
     .then((response) => {
       let products = response;
@@ -19,31 +38,34 @@ const getData = (api) => {
 };
 
 const loadData = () => {
-  getData(API);
+  const offset = addPage();
+  getData(offset);
 };
 
 const intersectionObserver = new IntersectionObserver(
   (entries) => {
     // logic...
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadData();
+      }
+    });
   },
   {
     rootMargin: "0px 0px 100% 0px",
   }
 );
 
-intersectionObserver.observe($observe);
-
-loadData();
-
 function Card(product) {
-  console.table(product);
-
   return `
+  <p>${product.id}</p>
   <article class="Card">
-    <img src="${product.images[0]}" />
+    <img src="${product.images[0]}" alt="${product.title}"/>
     <h2>
       ${product.title}
       <small>$ ${product.price}.00</small>
     </h2>
   </article>`;
 }
+
+intersectionObserver.observe($observe);
