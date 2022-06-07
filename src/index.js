50bbceceed56a05e512b96f5d75c5732cbe4120b
Luis Fernando Niño Gonzalez <cudoxnba@gmail.com>
2022-06-07 14:46:11 -0500

    Update index.js
    update

--- src/index.js
+++ src/index.js
@@ -8,8 +8,19 @@ const getData = api => {
     .then(response => {
       let products = response;
       let output = products.map(product => {
+        console.log(products);
+        /*let max = 0;
+        console.log(max);
+        console.log(product.id, max);*/
+        if(product.id > 4){
+            max++;
+            console.log(max);
+            return `<article class="Card" alt="${product.description}"><img src="${product.category.image}"/><h2>${product.title}<small>$ ${product.price}</small></h2></article>`;
+          }
         // template
-      });
+      });/*.filter(function(x){
+        return x !== undefined;
+      });*/
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
@@ -23,6 +34,7 @@ const loadData = () => {
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
+  loadData();
   // logic...
 }, {
   rootMargin: '0px 0px 100% 0px',
