d9a886c96651a0f98793af2b7d4c7dd2dcd0f34e
Ruben Solis <rsoliss@gmail.com>
2022-06-07 17:00:00 -0500

    solve challenges

--- src/index.js
+++ src/index.js
@@ -4,50 +4,55 @@ const API = 'https://api.escuelajs.co/api/v1/products';
 const LIMIT = 10;
 const OFFSET = 5;
 const TOTAL_PRODUCTS = 200;
+const PAGINATION_KEY = 'pagination'
 
-localStorage.clear();
+localStorage.removeItem(PAGINATION_KEY);
+let pagination = OFFSET;
 
-const getData = api => {
-  let pagination = parseInt(localStorage.getItem('pagination') || OFFSET) ;
-
-  fetch(`${api}?offset=${pagination}&limit=${LIMIT}`)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        return `<article class="Card">
-                  <img src="${product.category.image}" alt="#${product.title}"/>
-                  <h2>
-                    ${product.title}
-                    <small>$ ${product.price}</small>
-                  </h2>
-                </article>`
+const getData = async(api) => {
+  try {
+    if (pagination >= TOTAL_PRODUCTS) {
+      alert('Todos los productos Obtenidos');
+      intersectionObserver.disconnect();
+      return;
+    }
+  
+    const response = await fetch(`${api}?offset=${pagination}&limit=${LIMIT}`);
+    const products = await response.json();
+    
+    let output = products.map(product => {
+      return `<article class="Card">
+                <img src="${product.category.image}" alt="#${product.title}"/>
+                <h2>
+                  ${product.title}
+                  <small>$ ${product.price}</small>
+                </h2>
+              </article>`
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+  
+    localStorage.setItem(PAGINATION_KEY, pagination);
+    pagination += products.length;
 
-      pagination += products.length;
-      localStorage.setItem('pagination', pagination);
-
-      if (pagination >= TOTAL_PRODUCTS) {
-        alert('Todos los productos Obtenidos');
-        intersectionObserver.disconnect();
-      }
-
-    })
-    .catch(error => console.log(error));
+  } catch(error) {
+    console.log(error)
+  }
 }
 
-const loadData = async () => {
+const loadData = async() => {
   await getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
-  }
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
