86720ee6ebfbb1f69340036e5466450f6999bfe7
irving <irvoma@gmail.com>
2022-06-07 14:36:06 -0500

    feat: solution to the first problem
    - add getPage and setPage functions for pagination
    - add limit query string to API endpoint
    - change from map to reduce in getData function in order to show results

--- src/index.js
+++ src/index.js
@@ -1,29 +1,52 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API = 'https://api.escuelajs.co/api/v1/products?limit=10';
 
-const getData = api => {
+const getData = (api, offset) => {
+  api += `&offset=${offset}`;
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
+      let output = products.reduce((template, product) => {
         // template
-      });
+        return template += `
+        <article class="Card">
+          <img src="${product.images[0]}" />
+          <h2>
+          <b>[${product.id}]</b>
+          ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>
+        `;
+      }, '');
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
+const getPage = () => {
+  let page = window.localStorage.getItem('pagination');
+  return page ? parseInt(page): 4;
+}
+
+const setPage = page => window.localStorage.setItem('pagination', page);
+
 const loadData = () => {
-  getData(API);
+  let page = getPage();
+  getData(API, page);
+  setPage(page + 10);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  let entry = entries[0];
+  if (!entry.isIntersecting) return;
+  loadData();
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
