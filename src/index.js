03add5d9fe1a1a144c4cf208dc49db4bc6975d15
Diana <diana@multiplica.com>
2022-06-07 15:42:40 -0500

    feat(js-challenge): reto platzi

--- src/index.js
+++ src/index.js
@@ -1,31 +1,77 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
+const API = 'https://api.escuelajs.co/api/v1/products';
+const KEY = 'initial';
+const INITIAL = '4';
+const PRODUCTS_PER_PAGE = 10;
+
+const init = function () {
+  intersectionObserver.observe($observe);
+  localStorage.setItem(KEY, INITIAL);
+};
+
+const getData = (api) => {
   fetch(api)
-    .then(response => response.json())
-    .then(response => {
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
+
+      if (products.length === 0) {
+        alert('Todos los productos Obtenidos');
+        destroyed();
+        return;
+      }
+      updateInitialValue(products.slice(-1)[0].id);
+
+      let output = products.map((e) => {
+        return (
+          "<article class='Card'><img src='" +
+          e.images[0] +
+          "'/><h2>" +
+          e.title +
+          ' ' +
+          '<small>$ ' +
+          e.price +
+          '</small></h2></article>'
+        );
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
+};
+
+function updateInitialValue(value) {
+  localStorage.setItem(KEY, value);
 }
 
-const loadData = () => {
-  getData(API);
+async function loadData() {
+  getData(API + '?offset=' + localStorage.getItem(KEY) + '&limit=' + PRODUCTS_PER_PAGE);
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadData();
+      }
+    });
+  },
+  {
+    rootMargin: '0px 0px 100% 0px',
+  }
+);
 
-intersectionObserver.observe($observe);
+const destroyed = function () {
+  intersectionObserver.disconnect();
+};
+
+window.onbeforeunload = function () {
+  localStorage.removeItem(KEY);
+  return '';
+};
+
+init();
