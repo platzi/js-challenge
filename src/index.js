941260a503941cfeb8e8d8ebe5ea5f8585679f2d
Daagaa <diego.miranda.c@usach.cl>
2022-06-07 18:21:47 -0400

    Solucion 1, 2, 3 y 4. 5to listo igual

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+localStorage.setItem('pagination', 1);
 
-const getData = api => {
-  fetch(api)
+const getData = async (api,pagination) => {
+  await fetch(api+`?offset=${4+10*pagination}&limit=10`)
     .then(response => response.json())
     .then(response => {
       let products = response;
+      console.log(products);
       let output = products.map(product => {
         // template
+        return `<article class="Card">
+          <img loading="lazy" src="${product.category.image}" />
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+          <p>${product.description}</p>
+        </article>`;
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      //Implementar mensaje: "Todos los productos Obtenidos".
+      if(response.length < 10){
+        $observe.innerHTML = `<h1> Todos los productos Obtenidos </h1> `;
+        intersectionObserver.disconnect()
+      }
     })
     .catch(error => console.log(error));
+  
 }
 
-const loadData = () => {
-  getData(API);
+//Actualiza la función loadData() a Async/Await
+const loadData = async (pagination) => {
+  await getData(API,pagination);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  //Guarda en localStorage la posición inicial ("pagination") y actualízala en cada petición nueva para traer los siguientes productos
+  if (entries[0].isIntersecting){
+    let pagination = localStorage.getItem('pagination');
+    loadData(pagination-1)
+    localStorage.setItem('pagination', parseInt(pagination)+1);
+  }
+
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
+
+window.onbeforeunload = function(){
+  localStorage.clear();
+};
 
 intersectionObserver.observe($observe);
