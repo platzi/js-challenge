60b434ce692fc6f6d84b0bb64bc2092ca1f9fdbd
Osman Beltran <osmancadc@hotmail.com>
2022-06-07 10:32:02 -0500

    Solve to the JS test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+var offset = 4
 
-const getData = api => {
-  fetch(api)
+localStorage.setItem('pagination', 4);
+
+const getData = async (api) => {
+  fetch(`${api}?limit=10&offset=${offset}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
+        return `<article class="Card">
+                  <img src="${product.images[0]}" alt="Image of the product #${product.id}"/>
+                  <h2>
+                    ${product.title}
+                    <small>$ ${product.price}</small>
+                  </h2>
+                </article>`
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -18,12 +27,21 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  await getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  offset = parseInt(localStorage.getItem('pagination'));
+  if (offset >= 200) {
+    alert("Todos los productos Obtenidos")
+    intersectionObserver.disconnect()
+    return
+  }
+  if (entries[0].isIntersecting && Math.floor(entries[0].intersectionRatio) === 1) {
+    (async () => await loadData())();
+    localStorage.setItem('pagination', offset + 10);    
+  }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
