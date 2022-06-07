95f6e0353eb32a44740c19bd5aac6493b6c8dfb0
Leonardo Omaña Cortés <leonardoomanacortes@Leonardos-MacBook-Pro.local>
2022-06-07 17:27:27 -0500

    Fix challenge

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ const LIMIT_PRODUCTS = 10;
 const INIT_PAGINATION = 5;
 
 localStorage.setItem("pagination", INIT_PAGINATION);
-let pagination = localStorage.getItem("pagination");
 
 const getData = (api) => {
+  let pagination = localStorage.getItem("pagination");
+  if (document.getElementsByTagName("article").length >= 10) {
+    localStorage.setItem("pagination", Number(pagination) + LIMIT_PRODUCTS);
+    pagination = localStorage.getItem("pagination");
+  }
   fetch(`${api}?offset=${pagination}&limit=${LIMIT_PRODUCTS}`)
     .then((response) => response.json())
     .then((response) => {
@@ -20,21 +24,21 @@ const getData = (api) => {
 
         if (id === 201) {
           return `
-            <h2>Todos los productos optenidos</h2>
+          <h2>Todos los productos optenidos</h2>
           `;
         }
 
         if (!images.some((image) => image.startsWith("https"))) return null;
 
         return `
-          <article class="Card">
-            <img src="${images[0]}" alt="Imágen del artículo ${title}" />
-            <h2>
-              ${title}
-              <small>$ ${price}</small>
-            </h2>
-          </article>
-          `;
+        <article class="Card">
+        <img src="${images[0]}" alt="Imágen del artículo ${title}" />
+        <h2>
+        ${title}
+        <small>$ ${price}</small>
+        </h2>
+        </article>
+        `;
       });
       let newItem = document.createElement("section");
       newItem.classList.add("Items");
@@ -52,7 +56,6 @@ const intersectionObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        // localStorage.setItem("pagination", Number(pagination) + LIMIT_PRODUCTS);
         loadData();
       }
     });
