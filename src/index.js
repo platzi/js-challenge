6263e8f6db2036225ffd10f3408a3c90b4e64eae
carlosg <gtrujilloca@gmail.com>
2022-06-07 12:23:29 -0500

    ADD: First version

--- src/index.js
+++ src/index.js
@@ -1,31 +1,75 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+let offset = JSON.parse(localStorage.getItem('initialPage') || '5');
+const limit = 10;
+const newItem = document.createElement('section');
+newItem.classList.add('Items');
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+
+window.onbeforeunload = () => {
+  localStorage.clear();
+}
+
+const getData = async api => {
+  try {
+    const response = await fetch(api);
+    const products = await response.json();
+    const output = products.map(product => {
+      // template
+      const article = document.createElement('article');
+      article.classList.add('Card');
+
+      article.innerHTML = `
+        <article class="Card">
+          <img src="${product.images[0]}" />
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>
+      `
+      return article.innerHTML;
+    });
+    newItem.innerHTML += output.join('');
+    $app.appendChild(newItem);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const loadData = () => {
-  getData(API);
+  getData(`${API}?offset=${offset}&limit=${limit}`);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  console.log(entries);
+  if (offset >= 190) {
+    observe.innerHTML = `
+      <h2>Todos los productos Obtenidos</h2>
+    `
+    intersectionObserver.unobserve($observe);
+    return;
+  }
+
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+      saveLocalStorage();
+      offset += 10;
+    }
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
+  threshold: 1.0
 });
 
+const saveLocalStorage = () => {
+  localStorage.setItem('initialPage', JSON.stringify(offset));
+}
+
 intersectionObserver.observe($observe);
+
+
+// loadData();
