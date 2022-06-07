8286686d4a91f49d0fd666dd6809b429c02f8c88
desarrolladorp09 <codigop09@gmail.com>
2022-06-07 16:13:39 -0500

    Se resuelven los primeros 2 problemas y un poco del tercero. Los test de Cypress se logran cumplir, se corrige clase Item que debería ser Items. Falta actualizar función loadData() para que sea Async y el mensaje de todos los productos Obtenidos.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
-
-const getData = api => {
-  fetch(api)
+localStorage.clear();
+const getData = (api,offset) => {
+  offset==null?offset=5:offset;
+  fetch(`${api}?offset=${offset}&limit=10`)
     .then(response => response.json())
     .then(response => {
+      localStorage.setItem('pagination',offset);
       let products = response;
       let output = products.map(product => {
-        // template
+        return `<article class="Card">
+        <img src="${product.category.image}" />
+        <h2>
+          ${product.title}
+          <small>${product.price}</small>
+        </h2>
+      </article>`
       });
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
@@ -19,11 +27,18 @@ const getData = api => {
 }
 
 const loadData = () => {
+  
   getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const actualPagination =localStorage.getItem('pagination');
+      const offset = (actualPagination!=null)? Number(actualPagination)+10 : 5;
+      getData(API,offset);
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
