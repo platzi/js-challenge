b60805fed6a1bfd17720cd48a63d248e7cc3cc81
santiago-jv <santiagoolayojv@gmail.com>
2022-06-07 11:41:34 -0500

    changes applied

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const $observe = document.getElementById("observe");
 const $message = document.getElementById("message");
 const API = "https://api.escuelajs.co/api/v1/products";
 
-localStorage.removeItem('pagination')
-let offset = 5;
-localStorage.setItem('pagination', offset)
+localStorage.clear();
 
-const getData = (async (api) => {
-  const response = await fetch(`${api}?offset=${offset}&limit=10`)
+
+const getData = (async (api,offset,limit=10) => {
+  const response = await fetch(`${api}?offset=${offset}&limit=${limit}`)
   const data = await response.json()
+  localStorage.setItem('pagination', offset)
       let products = data;
       let output = products.map((product) => {
         return `
@@ -22,49 +22,32 @@ const getData = (async (api) => {
       </article>
         `;
 	  })
-      
+	  
       let newItem = document.createElement("section");
       newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+
+	  if(products.length < 10) {
+		$message.style.display = 'block'
+		intersectionObserver.disconnect()
+	}
 })
 const loadData = () => {
   getData(API);
 };
 
-loadData()
+
 const intersectionObserver = new IntersectionObserver(
 async  (entries) => {
+
 		entries.forEach(async(entry)=> {
 			if(entry.isIntersecting) {
-				const response = await fetch(`${API}?offset=${offset}&limit=10`)
-				const data = await response.json()
-				const products = data
-
-				if(products.length >= 10) {
-					let output = products.map((product) => {
-						return `
-						<article class="Card">
-						<img src="${product.category.image}" />
-						<h2>
-						  ${product.title}
-						  <small>$ ${product.price}</small>
-						</h2>
-					  </article>
-						`;
-					  })
-					let newItem = document.createElement("section");
-					newItem.classList.add("Items");
-					newItem.innerHTML = output;
-					$app.appendChild(newItem);
-	
-					offset=offset + 10
-					localStorage.setItem('pagination', offset)
-				}
-				else {
-					$message.style.display = 'block'
-					intersectionObserver.disconnect()
-				}
+				const offsetFromLS = localStorage.getItem('pagination') 
+				const offset=offsetFromLS ? Number(offsetFromLS) + 10 : 5
+				await getData(API, offset)
+				
+				
 			}
 		})
   },
