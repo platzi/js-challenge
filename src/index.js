a296a9f67a151c06e9d1e1b0dfeaccbf40e5d30c
Nicolas Valentinuzzi <nicovalentinuzzi@gmail.com>
2022-06-07 17:12:39 -0300

    challenges 1 to 4

--- src/index.js
+++ src/index.js
@@ -1,31 +1,83 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+const setPaginationInStorage = (offset, limit) => {
+  const pagination = {
+    offset,
+    limit,
+  };
+  localStorage.setItem("pagination", JSON.stringify(pagination));
+};
+
+const getData = (api) => {
+  const pagination = JSON.parse(localStorage.getItem("pagination"));
+  const apiUrl = `${api}?offset=${pagination.offset}&limit=${pagination.limit}`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      let output = products
+        .map(
+          (product) =>
+            `
+            <article class="Card">
+              <img src="${product.images[0]}" alt="${product.title}">
+              <h2>
+                ${product.title}
+                <small>${product.price}</small>
+              </h2>
+            </article>
+          `
+        )
+        .join("");
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
+};
+
+const loadData = async (offset, limit) => {
+  setPaginationInStorage(offset, limit);
+  if (offset <= 200) {
+    await getData(API);
+  } else {
+    console.log(offset);
+    intersectionObserver.unobserve($observe);
+    let endOfItemsMessage = document.createElement("section");
+    endOfItemsMessage.classList.add("Center");
+    endOfItemsMessage.innerHTML = `
+      <h1>Todos los productos Obtenidos</h1>
+    `;
+    $app.appendChild(endOfItemsMessage);
+  }
+};
+
+let intersectionObserver;
+
+function createObserver() {
+  intersectionObserver = new IntersectionObserver(
+    (entries) => {
+      const actualPagination = JSON.parse(localStorage.getItem("pagination"));
+      loadData(
+        actualPagination.offset + actualPagination.limit,
+        actualPagination.limit
+      );
+    },
+    {
+      rootMargin: "0px 0px 100% 0px",
+    }
+  );
+  intersectionObserver.observe($observe);
 }
 
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
-
-intersectionObserver.observe($observe);
+window.addEventListener(
+  "load",
+  (event) => {
+    loadData(5, 10);
+    createObserver();
+  },
+  false
+);
