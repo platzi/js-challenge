a2bb2da6e53efeb7fee2312baed30ef82810683c
yurixtugal <yurixtugal12@gmail.com>
2022-06-07 13:55:01 -0500

    Prueba Platzi

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
+const $finalMessage = document.getElementById('finalMessage');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const LIMIT = 10;
+const LIMIT_API = 200;
 
-const getData = api => {
-  fetch(api)
+localStorage.clear();
+localStorage.setItem("offSet",4);
+
+const getData =   (api) => {
+
+   fetch(api) 
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
+      let output = products.map((product) => 
+          '<article class="Card">'+
+              '<img src="'+product.category.image+'" />'+
+              '<h2>'+
+              product.title+
+                '<small>$'+product.price+'</small>'+
+              '</h2>'+
+          '</article>'    
+      
+      );
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      
+      let offsetTmp = parseInt(localStorage.getItem("offSet"));
+      if (offsetTmp > LIMIT_API){
+        let newMessageItem = document.createElement('message');
+        newMessageItem.innerHTML = "<h1>Todos los productos fueron mostrados</h1>"
+        $finalMessage.append(newMessageItem);
+        intersectionObserver.disconnect();
+      }
+
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async  () => {
+  let offset  = parseInt(localStorage.getItem("offSet"));
+  localStorage.setItem("offSet",offset+LIMIT);
+   await getData(API+"?offset="+offset+"&limit="+LIMIT);
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+const intersectionObserver = new IntersectionObserver( (entries) => {
+  if (!entries[0].isIntersecting) return;
+  loadData();
+   
 }, {
-  rootMargin: '0px 0px 100% 0px',
+  rootMargin: '0px 640px 100% 0px',
 });
+
 
 intersectionObserver.observe($observe);
