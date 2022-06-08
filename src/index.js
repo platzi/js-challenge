beb9ef70609c707b5e7d8041fbccd915de1494f1
Eimer Castro <Eimer.Castro@nordstrom.com>
2022-06-07 21:06:48 -0500

    Primer problema solucionado.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const $app = document.getElementById('app');
+const $items = document.getElementById('Items');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+let $offset = 5;
+const $limit = 10;
+const RESULT_COUNT = 200;
+
+const updateLocalStorage = () => {
+  if (localStorage.getItem('pagination')) {
+    $offset = parseInt(localStorage.getItem('pagination'));
+    if ($offset >= RESULT_COUNT) {
+      $offset = 190;
+      intersectionObserver.unobserve($observe);
+    } else {
+      $offset += $limit;
+      if ($offset >= RESULT_COUNT) {
+        intersectionObserver.unobserve($observe);
+      }
+    }
+  }
+  localStorage.setItem('pagination', $offset);
+  return `?offset=${$offset}&limit=${$limit}`;  
+}
 
 const getData = api => {
-  fetch(api)
+  let $pagination = updateLocalStorage();
+  fetch(api + $pagination)
     .then(response => response.json())
     .then(response => {
       let products = response;
@@ -23,7 +45,11 @@ const loadData = () => {
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
