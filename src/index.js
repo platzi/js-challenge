3d06d5c10d337570fb610d7785031b989d84cce8
sergio valadez <sergioval@gmail.com>
2022-06-08 16:16:32 -0500

    Add infinite scrolling

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10';
+const API = 'https://api.escuelajs.co/api/v1/products?offset=6&limit=10';
 
-const getData = async () => {
-  try {
-    const response = await fetch(API)
-    console.log(response);
-
-    if (response.ok) {
-      const data = await response.json();
-      
-
-      data.forEach(product => {
-        console.log(product.title);
-      })
-        
-    } else {
-      console.log('Error');
-    }
-  } catch (error) {
-    console.log(error);    
-  }
-
+const getData = api => {
+  fetch(api)
+    .then(response => response.json())
+    .then(response => {
+      let products = response;
+      let output = products.map(product => {
+        product.price = product.price.toLocaleString('en-EU', { style: 'currency', currency: 'USD' });
+        return `
+          <div class="Card">
+            <div class="card-body">
+              <h2 class="card-title">${product.title}</h2>
+              <img src="${product.category.image}" alt="${product.title}" class="card-img-top">
+              <p class="card-text">${product.description}</p>
+              <p class="card-text">${product.price}</p>
+            </div>
+          </div>
+        `;	
+      });
+      let newItem = document.createElement('section');
+      newItem.classList.add('Item');
+      newItem.innerHTML = output;
+      $app.appendChild(newItem);
+    })
+    .catch(error => console.log(error));
 }
-getData();
+
+
+const loadData = () => {
+  getData(API);
+}
+
+const intersectionObserver = new IntersectionObserver(entries => {
+  entries.push(entries[0]);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  });
+}, {
+  rootMargin: '0px 0px 100% 0px',
+});
+
+intersectionObserver.observe($observe);
