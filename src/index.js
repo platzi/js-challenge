d0b227aca38f07d55187f2366764d240ba99c25e
Joaquín Ignacio Ossandón Gómez <58451051+Gh0stRaccoon@users.noreply.github.com>
2022-06-08 13:59:28 -0400

    fix: se muestran los elementos y scrollinfinito

--- src/index.js
+++ src/index.js
@@ -1,31 +1,63 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+const limit = 10;
+localStorage.setItem("pagination", 5);
 
-const loadData = () => {
-  getData(API);
-}
+const getData = async (api) => {
+  try {
+    const response = await fetch(
+      `${api}?offset=${localStorage.getItem("pagination")}&limit=${limit}`
+    );
+    const products = await response.json();
+    let output = products.map(
+      (product) =>
+        `<article class="Card">
+            <img src=${product.images[0]} />
+            <h2>
+              ${product.title}
+              <small>$${product.price}</small>
+            </h2>
+          </article>`
+    );
+    let newItem = document.createElement("section");
+    newItem.classList.add("Items");
+    newItem.innerHTML = output.join("");
+    $app.appendChild(newItem);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const loadData = async () => {
+  await getData(API);
+};
+
+const intersectionObserver = new IntersectionObserver(
+  (entries, self) => {
+    if (entries[0].isIntersecting && window.scrollY !== 0) {
+      const actualPosition = +localStorage.getItem("pagination");
+      const nextPosition = actualPosition + limit;
+      localStorage.setItem("pagination", nextPosition);
+      getData(API);
+    }
+    if (localStorage.getItem("pagination") >= 200) {
+      self.unobserve($observe);
+      let noMoreItems = document.createElement("p");
+      noMoreItems.innerText = "Todos los productos Obtenidos";
+      let iconItem = document.createElement('span');
+      iconItem.innerText = " 💚"
+      noMoreItems.appendChild(iconItem)
+      noMoreItems.classList.add("NoMoreItems");
+      $app.appendChild(noMoreItems);
+    }
+  },
+  {
+    rootMargin: "0px 0px 200px 0px",
+  }
+);
+
+loadData();
 
 intersectionObserver.observe($observe);
