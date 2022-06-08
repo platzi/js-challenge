2c4bec8ebf3d7e04cfff2444e6dfcf38822bf3be
Carlos Andres Gonzalez <carlos.gs.andres@gmail.com>
2022-06-08 16:30:58 -0500

    First CHallenge

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
-
-const getData = api => {
-  fetch(api)
+const $limit = 10;
+const $offset = 4;
+const getData = (api, limit, offset) => {
+  console.log('getData')
+  fetch(`${api}?limit=${limit}&offset=${offset}`)
     .then(response => response.json())
     .then(response => {
+      
       let products = response;
+      console.log(products)
       let output = products.map(product => {
         // template
+        return `
+        <h2>${product.id} - ${product.title}</h2>`;
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      let newItem = document.createElement("section");
+      newItem.classList.add("Item");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
+
+
 
 const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+  let offset = window.localStorage.getItem("pagination")
+  ? parseInt(window.localStorage.getItem("pagination")) + $limit
+  : $offset;
+  getData(API, $limit, offset);
+  window.localStorage.setItem("pagination", offset);
+};
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) loadData();
+    });
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
