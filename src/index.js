661ad5a719860357a78b6eabf38132526ee0e610
ocmv <orianacmv@gmail.com>
2022-06-07 21:29:58 -0400

    primer problema

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const limit = 10;
+
+function setPagination(){
+  let pagination;
+  if(localStorage.getItem('pagination') === null){
+    pagination = 5;
+  }else{
+    pagination = Math.trunc(localStorage.getItem('pagination')) + limit;
+  }
+  localStorage.setItem('pagination', pagination);
+  return pagination;
+}
 
 const getData = api => {
-  fetch(api)
+  const pagination = setPagination();
+  fetch(`${api}?offset=${pagination}&limit=${limit}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
+      console.log(products);
       let output = products.map(product => {
         // template
       });
@@ -23,7 +37,11 @@ const loadData = () => {
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
