0ebce9cac56e61856af87084199d3a66ce9ea93e
SKOL web development <perezcatriel@gmail.com>
2022-06-08 11:37:20 -0300

    Terminado

--- src/index.js
+++ src/index.js
@@ -1,31 +1,57 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+const limit = 10;
+let intersect = 1;
+let offset = 5;
 
-const loadData = () => {
-  getData(API);
-}
+const addElement = (items) => {
+	return items.map((item) => {
+		return `<article class="Card"><img src=${item.images[0]} /><h2>${item.id} - ${item.title} <strong>$${item.price}</strong></h2></article>`;
+	});
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const getData = async (api) => {
+	const response = await fetch(api);
+	const products = await response.json();
+
+	localStorage.removeItem("pagination");
+
+	try {
+		let output = addElement(products);
+
+		let newItem = document.createElement("section");
+		newItem.classList.add("Items");
+		newItem.innerHTML = output.join("");
+		$app.append(newItem);
+
+		localStorage.setItem("pagination", parseInt(offset));
+		offset += 10;
+	} catch {
+		console.error(err);
+	}
+};
+
+const loadData = async () => {
+	let pageable = "?offset=" + offset + "&limit=" + limit;
+	getData(API + pageable);
+};
+
+const intersectionObserver = new IntersectionObserver(
+	(entries) => {
+		// logic...
+		if (entries[0].isIntersecting) {
+			loadData();
+		} else if (offset >= 200) {
+			const info = `<p class="Info">Todos los productos cargados</p>`;
+			$observe.innerHTML = info;
+			intersectionObserver.disconnect();
+		}
+	},
+	{
+		rootMargin: "0px 0px 100% 0px",
+	}
+);
 
 intersectionObserver.observe($observe);
