2873f75416984b116f1d1e21fa168a495b933152
Javier Mansilla <38993747+javierelhuno@users.noreply.github.com>
2022-06-08 16:06:01 -0500

    Javier Mansilla Resueltos

--- src/index.js
+++ src/index.js
@@ -1,31 +1,71 @@
+
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+let pagination = localStorage.getItem('pagination')
+localStorage.setItem('pagination', 5);
+window.addEventListener('beforeunload', () => {
+  localStorage.removeItem('pagination');
+})
 
+document.addEventListener('DOMContentLoaded', () => {
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: [0.0, 0.25, 0.5, 0.75, 1.0]
+    };
+    function callback(entries) {
+      if (document.getElementById('no-more-products') !== null) {
+        observer.unobserve($observe);
+      }
+      loadData();
+    }
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe($observe);
+  }
+);
+
+console.log("Ejecutamos el script")
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      let pagination = localStorage.getItem('pagination')
+      if (products.length > 0) {
+        let output = products.map(product => {
+          return `
+            <article class="Card">
+              <img src="${product.category.image}" alt="${product.name}">
+              <h2>${product.title} <small>$ ${product.price}</small></h2>
+              <p>${product.description}</p>
+            </article>
+          `
+        });
+        var newPagination = parseInt(pagination)
+        localStorage.setItem('pagination',newPagination + 10 )
+        let newItem = document.createElement('section');
+        newItem.classList.add('Item');
+        newItem.innerHTML = output;
+        $app.appendChild(newItem);
+      }
+      else {
+        if (document.getElementById('no-more-products') === null) {
+          let newItem = document.createElement('h1');
+          newItem.classList.add('h1');
+          newItem.id = 'no-more-products';
+          newItem.innerHTML = "Todos los productos Obtenidos";
+          $app.appendChild(newItem);
+        }
+      }
+      
+      
     })
     .catch(error => console.log(error));
+  
 }
 
-const loadData = () => {
+async function loadData() {
+  let pagination = localStorage.getItem('pagination')
+  let API = `https://api.escuelajs.co/api/v1/products?limit=10&offset=${pagination}`;
   getData(API);
 }
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
-
-intersectionObserver.observe($observe);
