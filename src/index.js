5271c0c54ef4d9a181c3ffe6b66cce596a1c2c49
Ernesto Galdamez <ernesto_galdamez@hotmail.com>
2022-06-07 21:41:28 -0600

    [Update] Code Assignment complete

--- src/index.js
+++ src/index.js
@@ -2,30 +2,76 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+//Iniciamos el producto 5 y obteniendo los siguientes 10 productos.
+localStorage.setItem('paginationIndex', 4);
+const PRODUCTS_TO_FETCH = 10;
+
+const $itemsContainer = document.createElement('div');
+$itemsContainer.classList.add('items');
+$app.appendChild($itemsContainer);
+
+//Cuando la última petición sea ejecutada debes de mostrar un mensaje "Todos los productos Obtenidos", a su vez debes de destruir el intersection observer.
+const $completeMessage = document.createElement('div');
+$completeMessage.classList.add('completeMessage');
+$completeMessage.innerHTML = 'Todos los productos Obtenidos'
+
+const getData = (api, index, limit) => 
+  fetch(`${api}?offset=${index}&limit=${limit}`)
+  .then(response => response.json())
+  .then(response => response)
+  .catch(error => console.log(error));
+
+const loadData = async () => {
+  const products = await getData(API, localStorage.getItem('paginationIndex'), PRODUCTS_TO_FETCH);
+  console.log(products);
+  products.map(product => $itemsContainer.appendChild(getCardElement(product)));
+  localStorage.setItem('paginationIndex', PRODUCTS_TO_FETCH + +localStorage.getItem('paginationIndex'));
+  if (products.length < 10) {
+    handleObserveStatus(false);
+    $app.appendChild($completeMessage);
+  }
 }
 
-const loadData = () => {
-  getData(API);
+const getCardElement = (product) => {
+  const $card = document.createElement('article');
+  $card.classList.add('card');
+  $card.innerHTML =`
+    <img src="${product.images[0]}" />
+    <h2>
+      ${product.title}
+    </h2>
+    <p>$ ${product.price}</p>
+    
+  `;
+  return $card;
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      loadData()
+    }
+  })
 }, {
-  rootMargin: '0px 0px 100% 0px',
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.25
 });
 
-intersectionObserver.observe($observe);
+const handleObserveStatus = status => {
+  if (status) {
+    console.log('Observer started');
+    intersectionObserver.observe($observe);
+  }
+  else {
+    console.log('Observer stopped');
+    intersectionObserver.unobserve($observe);
+  }
+}
+
+handleObserveStatus(true);
+
+//Eliminamos el storage (Tercer Problema, item 2)
+window.onbeforeunload = () => {
+  localStorage.removeItem('paginationIndex');
+};
