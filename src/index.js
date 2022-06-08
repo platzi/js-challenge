ef26927e2e699b9da6cea6be6e4b1eeb2a5a2109
Sergio Sinuco <ssinuco@gmail.com>
2022-06-08 16:35:01 -0500

    Solucion primer problema

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const getData = api => {
-  fetch(api)
+  const limit = 10;
+  const offset = localStorage.getItem('offset') ? parseInt(localStorage.getItem('offset')) + limit : 5;
+
+  fetch(`${api}?offset=${offset}&limit=${limit}`)
     .then(response => response.json())
     .then(response => {
       let products = response;
@@ -14,6 +17,8 @@ const getData = api => {
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+
+      localStorage.setItem('offset', offset);
     })
     .catch(error => console.log(error));
 }
@@ -23,7 +28,10 @@ const loadData = () => {
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  const target = entries[0];
+  if (target.isIntersecting) {
+    loadData();
+  }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
