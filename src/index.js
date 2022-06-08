30bb6ef26f9b6af46e1ed621511dbb1c740ce27a
barduck <sebastiancamara@gmail.com>
2022-06-07 23:12:09 -0300

    Challenge done.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,66 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+// Clear local storage
+localStorage.clear();
 
-const loadData = () => {
-  getData(API);
-}
+const getData = async (api) => {
+	// API url
+	const url = api.url + "?offset=" + api.offset + "&limit=" + api.limit;
+	localStorage.setItem("pagination", api.offset);
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+	// Card
+	const data = await fetch(url);
+	const products = await data.json();
+	const output = products.map(
+		(product) => `
+			<article class="Card">
+				<img src="${product.images[0]}" alt="${product.name}">
+				<h2>
+					${product.title}
+					<small>$ ${product.price}</small>
+				</h2>
+			</article>
+		`
+	);
+
+	// Insert item (product)
+	let newItem = document.createElement("section");
+	newItem.classList.add("Items");
+	newItem.innerHTML = output.join("");
+	$app.appendChild(newItem);
+
+	// Scroll completed
+	if (products.length < api.limit) {
+		let message = document.getElementById("message");
+		// Avoid duplicated elements
+		if (!message) {
+			message = document.createElement("p");
+			message.setAttribute("id", "message");
+			$app.appendChild(message);
+		}
+		message.innerHTML = "Todos los productos obtenidos.";
+		intersectionObserver.unobserve($observe);
+	}
+};
+
+const loadData = async (offset, limit) => {
+	await getData({
+		url: API,
+		offset: offset,
+		limit: limit,
+	});
+};
+
+const intersectionObserver = new IntersectionObserver(
+	(entries) => {
+		const offset = localStorage.getItem("pagination") ? parseInt(localStorage.getItem("pagination")) + 10 : 5;
+		loadData(offset, 10);
+	},
+	{
+		rootMargin: "0px 0px 100% 0px",
+	}
+);
 
 intersectionObserver.observe($observe);
