5dfee5fc700593fe3b86ad3762c7ec2dcea8dbdc
matisyk <matisyk@hotmail.com>
2022-06-08 12:53:47 -0300

    Last Changes

--- src/index.js
+++ src/index.js
@@ -1,56 +1,58 @@
 const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = `https://api.escuelajs.co/api/v1/products`;
+ const $observe = document.getElementById('observe');
+ const API = 'https://api.escuelajs.co/api/v1/products';
 
-localStorage.removeItem('pagination');
-localStorage.setItem('pagination', 5);
-
-const getData = api => {
-  fetch(api + `?offset=${localStorage.getItem('pagination')}&limit=10`)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => `
-         <article class="Card">
-           <img src=${product.category.image} />
-           <h2>
-             ${product.title}
-             <small>$ ${product.price}</small>
-           </h2>
-         </article>
-         `);
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output.join(' ');
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
-
-const loadData = async () => {
-  await getData(API);
-}
-
-let page = 0
-
-const intersectionObserver = new IntersectionObserver(entries => { 
-  if (entries[0].isIntersecting) {
-   page++
-    localStorage.setItem('page', page);
-
-    if (page > 1) {
-      localStorage.setItem('pagination', parseInt(localStorage.getItem('pagination')) + 10);
+ var objectPlace = {
+   clearStorage: (() => localStorage.clear())(),
+   paginationAPI: () => {    
+     const limit = 10;
+     const initOffset = 5;
+     const pagination =  localStorage.getItem('pagination');
+     const offset = pagination ? parseInt(pagination) + limit : initOffset;
+     localStorage.setItem('pagination', offset);
+     return `${API}?offset=${offset}&limit=${limit}`;  
+   },
+   templateCardHTML: ({images, title, price}) => {
+     return  `<article class="Card">
+                <img src="${images[0]}" />
+                <h2>${title}<small>$ ${price}</small></h2>
+              </article>`
+    },
+    validatePagination : (callBack) => {    
+      const pagination = localStorage.getItem('pagination');
+     if(pagination && parseInt(pagination) > 200) {
+       intersectionObserver.unobserve( $observe);
+       alert('Todos los productos Obtenidos');      
+     } else {
+       callBack();
+     }
     }
+ }
 
-    if (page > 20) {
-     alert("Todos los productos Obtenidos");
-     intersectionObserver.unobserve($observe);
-    }
+ const getData = api => {  
+   fetch(api)
+     .then(response => response.json())
+     .then(response => {
+       let products = response;
+       let output = products.map(product => {
+         return objectPlace.templateCardHTML(product);
+       });
+       let newItem = document.createElement('section');
+       newItem.classList.add('Items');
+       newItem.innerHTML = output.join("");
+       $app.appendChild(newItem);
+     })
+     .catch(error => console.log(error));
+ }
+ const loadData = async () => {  
+   await getData(objectPlace.paginationAPI());
+ }
 
-    loadData();
-  }
-  }, {
-    rootMargin: '0px 0px 100% 0px',
-  });
-
+ const intersectionObserver = new IntersectionObserver(entries => {
+   if (entries[0].isIntersecting) {
+     objectPlace.validatePagination(loadData);
+   }
+ }, {
+   rootMargin: '0px 0px 100% 0px',
+ });
 intersectionObserver.observe($observe);
