3d161001b5a04a72c49f2d6c0cf45f9abd8d9c72
Gastón Konstantinides <gaston.konstantinides@vortex-it.com>
2022-06-08 13:03:51 -0300

    feat: Challenge complete

--- src/index.js
+++ src/index.js
@@ -2,28 +2,66 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const DEFAULT_OFFSET = 4;
+const DEFAULT_LIMIT = 10;
+
+localStorage.setItem('offset', DEFAULT_OFFSET);
+
+const getData = async (api) => {
+  try {
+    const response = await fetch(api)
+    if(response.status !== 200) {
+      throw new Error(response.message)
+    }
+    return response.json()
+  } catch(err) {
+    throw new Error(err)
+  }
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async (offset = 4, limit = DEFAULT_LIMIT) => {
+  localStorage.setItem("offset", `${offset+limit}`);
+  
+  try {
+    let products = await getData(`${API}?offset=${offset}&limit=${limit}`)
+
+    let output = ``;
+    
+    products.forEach(product => {
+      output += `
+        <article id="product-${product.id}" class="Card">
+          <img src="${product.images[0]}" />
+          <h2>
+            ${product.title}
+            <small>$ ${product.price}</small>
+          </h2>
+        </article>
+      `;
+    });
+  
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = output;
+    $app.appendChild(newItem);
+
+    if(products.length < DEFAULT_LIMIT) {
+      let span = document.createElement('span')
+      span.classList.add('Message')
+      span.innerHTML = "Todos los productos Obtenidos"
+      $app.appendChild(span)
+      intersectionObserver.disconnect()
+    }
+
+  } catch(error) {
+    console.error(error)
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  if(entries[0].isIntersecting) {
+    let offset = parseInt(localStorage.getItem("offset"))||DEFAULT_OFFSET
+    loadData(offset);
+  }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
