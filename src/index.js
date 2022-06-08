92bc96612e70768a2aa899cb20d3a035f6ffaaf6
Luis Fernando Méndez <luisfernando.mendez5@gmail.com>
2022-06-07 20:29:50 -0400

    Update to validate initial state after 2000ms

--- src/index.js
+++ src/index.js
@@ -1,44 +1,45 @@
-localStorage.setItem('pagination', 5);
 const $app = document.getElementById("app");
 const $observe = document.getElementById("observe");
 const API = `https://api.escuelajs.co/api/v1/products`;
 
-
 function itemTemplate(data) {
-    return `
-    <article class="Card Item">
-    <img src=${data.images[0]} />
-    <h2>
-    ${data.title}
-    <small>$ ${data.price}</small>
-    </h2>
-    </article>
-    `;
+  return `
+  <article class="Card Item">
+  <img src=${data.images[0]} />
+  <h2>
+  ${data.title}
+  <small>$ ${data.price}</small>
+  </h2>
+  </article>
+  `;
 }
 
 const getData = (api) => {
+  localStorage.getItem('pagination') ?
+    localStorage.setItem('pagination', Number(localStorage.getItem('pagination') + 10)) :
+    localStorage.setItem('pagination', 5);
   const currentPage = localStorage.getItem('pagination');
   const paginatedApi = `${api}?offset=${(Number(currentPage))}&limit=10`;
   fetch(paginatedApi)
-    .then((response) => response.json())
-    .then((response) => {
-      let products = response;
-      if (products.length === 0) {
-        let endOfItems = document.createElement('h3')
-        endOfItems.classList.add('FinalMessage');
-        endOfItems.innerText = 'All the products have been shown'
-        $app.appendChild(endOfItems);
-        intersectionObserver.disconnect($observe);
-      }
-      let output = products && products.map((product) => {
-        return itemTemplate(product);
-      });
-      let newItem = document.createElement("section");
-      newItem.classList.add("Items");
-      newItem.innerHTML = output.join(' ');
-      $app.appendChild(newItem);
-    })
-    .catch((error) => console.error(error));
+  .then((response) => response.json())
+  .then((response) => {
+    let products = response;
+    if (products.length === 0) {
+      let endOfItems = document.createElement('h3')
+      endOfItems.classList.add('FinalMessage');
+      endOfItems.innerText = 'All the products have been shown'
+      $app.appendChild(endOfItems);
+      intersectionObserver.disconnect($observe);
+    }
+    let output = products && products.map((product) => {
+      return itemTemplate(product);
+    });
+    let newItem = document.createElement("section");
+    newItem.classList.add("Items");
+    newItem.innerHTML = output.join(' ');
+    $app.appendChild(newItem);
+  })
+  .catch((error) => console.error(error));
 };
 
 const loadData = async () => {
@@ -47,17 +48,6 @@ const loadData = async () => {
 
 const intersectionObserver = new IntersectionObserver(
   (entries) => {
-    window.onbeforeunload = function() {
-      localStorage.removeItem('pagination');
-      window.scrollTo(0, 0);
-    }
-
-    if (!entries[0].isIntersecting) {
-      const getCurrentPage = localStorage.getItem('pagination');
-      const newPage = Number(getCurrentPage) + 10;
-      localStorage.setItem('pagination', newPage)
-    }
-
     if (entries[0].isIntersecting) {
       loadData();
     }
@@ -65,6 +55,11 @@ const intersectionObserver = new IntersectionObserver(
   {
     rootMargin: "0px 0px 100% 0px",
   }
-);
+  );
+  
+window.onbeforeunload = function() {
+  localStorage.removeItem('pagination');
+  window.scrollTo(0, 0);
+}
 
 intersectionObserver.observe($observe);
