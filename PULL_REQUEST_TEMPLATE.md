8f9a6db155afb7abf4e9c7d40984ac0b655d4afe
Luis Eduardo Martinez Piñeiro <luis@MacBook-Pro-de-Plata.local>
2022-06-07 20:37:25 -0500

    Cambios reto

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,14 +2,14 @@
 
 Solución al reto:
 
-Nombre:
-Usuario Platzi:
-Correo Electronico:
+Nombre: Luis Eduardo Martinez Piñeiro
+Usuario Platzi: no tengo
+Correo Electronico: leduardo.mp@gmail.com
 
 ## Reto:
 
-- [ ] Primer problema
-- [ ] Segundo problema
-- [ ] Tercer problema
-- [ ] Cuarto Problema
-- [ ] Quinto Problema
+- [x] Primer problema
+- [x] Segundo problema
+- [x] Tercer problema
+- [x] Cuarto Problema
+- [x] Quinto Problema
--- src/index.js
+++ src/index.js
@@ -2,30 +2,55 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+let miStorage = window.localStorage;
+miStorage.clear();
+miStorage.setItem("pagination", 0);
+
+const getElements = product => {
+    const pagination = parseInt(miStorage.getItem("pagination"))
+    return product.id >= (10 * pagination) - 5 && product.id <= 4 + (10 * pagination);
+}
+
+const getData = async(api) => {
+    await fetch(api)
+        .then(response => response.json())
+        .then(response => {
+            let products = response;
+            let output = products.filter(getElements).map(product => {
+                return `
+                    <article class="Card">
+                        <img src="${product.images[0]}" alt="${product.description}" title="${product.title}" />
+                        <h2>${product.title}<small>$ ${product.price}</small></h2>
+                    </article>
+                `
+            });
+            let newItem = document.createElement('section');
+            newItem.classList.add('Item');
+            newItem.innerHTML = output;
+            $app.appendChild(newItem);
+        })
+        .catch(error => console.log(error));
 }
 
 const loadData = () => {
-  getData(API);
+    miStorage.setItem("pagination", parseInt(miStorage.getItem("pagination"))+1);
+    getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+    const paginacion = miStorage.getItem("pagination");
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            if (paginacion < 20) {
+                loadData();
+            } else {
+                alert("Todos los productos Obtenidos")
+               $observe.remove(); 
+            }
+        }
+    });
 }, {
-  rootMargin: '0px 0px 100% 0px',
+    rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
