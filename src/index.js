8491f4e41ddbc7cf79ed1f9190eab40d74d5f5e8
Ariel <aramirez@r2software.net>
2022-06-08 18:40:31 -0300

    reto challenge

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 const $list = document.getElementById('list');
 var offset = 0
-var limit = 0;
+    // var limit = 0;
 
 
 const getData = (api) => {
@@ -21,17 +21,19 @@ const getData = (api) => {
             newItem.classList.add('Item');
             newItem.innerHTML = output;
             $observe.appendChild(newItem);
+
+            let msg = document.createElement('h3');
+            // msg.innerHTML = "Todos los productos Obtenidos";
+            // $app.appendChild(msg);
             console.log("Todos los productos Obtenidos");
         })
         .catch(error => console.log(error));
 }
 
 async function loadData() {
-    let apiNew = API + '?offset=' + offset + '&limit=' + limit;
-    offset = limit
-    limit = limit + 10;
-    console.log(offset, limit);
-    await getData(apiNew);
+    let apiNew = API + '?offset=' + offset + '&limit=10';
+    offset = offset + 10;
+    getData(apiNew);
 }
 
 loadData();
