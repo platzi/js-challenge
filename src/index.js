2c126502154a48745aee070d99bd535c93be30f3
MarcosBarbas21 <84548780+MarcosBarbas21@users.noreply.github.com>
2022-06-08 10:02:20 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,31 +1,76 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
-
-const getData = api => {
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
+localStorage.setItem("pagination", 5);
+let offset = localStorage.getItem('offset') == 0;
+if (offset == 0) localStorage.setItem("pagination", 0);
+localStorage.setItem("limit", 10);
+let pagination = localStorage.getItem("pagination");
+let limit = localStorage.getItem("limit");
+let realAPI = API + `?offset=${pagination}&limit=${limit}`;
+const getData = async (api) => {
   fetch(api)
-    .then(response => response.json())
-    .then(response => {
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
-      let output = products.map(product => {
-        // template
+      let output = products.map((product) => {
+        return `<article class="Card">
+          <img src="${product.images[0]}"/>
+            <h2>
+            ${product.title}
+              <small>$ ${product.price}</small>
+            </h2>
+        </article>`;
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      let newItem = document.getElementsByClassName('Items');
+      if (newItem.length < 2) {
+        newItem = document.createElement("section");
+        newItem.classList.add("Items");
+        output.forEach(element => {
+          newItem.insertAdjacentHTML('beforeend', element);
+        })
+        $app.appendChild(newItem);
+      } else {
+        output.forEach(element => {
+          newItem[1].insertAdjacentHTML('beforeend', element);
+        })
+      }
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = async () => {
+  await getData(realAPI);
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio >= 0.8) {
+        if (offset >= 199) {
+          $app.insertAdjacentHTML(
+            "beforeEnd",
+            `<h1>Todos los productos Obtenidos</h1>`
+          );
+          intersectionObserver.disconnect();
+        } else {
+          loadData();
+          offset = Number(localStorage.getItem("offset")) + 10;
+          localStorage.setItem("offset", offset);
+          realAPI = API + `?offset=${offset}&limit=${limit}`;
+        }
+      }
+    });
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
+window.addEventListener("beforeunload", function () {
+  return localStorage.clear(), localStorage.setItem("offset", 0);
+});
+window.addEventListener("beforereload", function () {
+  return localStorage.clear(), localStorage.setItem("offset", 0);
+});
