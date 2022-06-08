7af6781878aec815589d4b32eb5e320451e66811
sergio valadez <sergioval@gmail.com>
2022-06-07 23:10:13 -0500

    Bring product title to console

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products/5';
+const API = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=10';
 
 const getData = async () => {
-  const response = await fetch(API);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(API)
+    console.log(response);
+
+    if (response.ok) {
+      const data = await response.json();
+      
+
+      data.forEach(product => {
+        console.log(product.title);
+      })
+        
+    } else {
+      console.log('Error');
+    }
+  } catch (error) {
+    console.log(error);    
+  }
+
 }
-const loadData = data => {
-  $app.innerHTML = 
-  `<div class="Card">
-    <div class="Item">
-      <h2>${data.title}</h2>
-      <img src="${data.category.image}" alt="${data.name}">
-      <p>${data.description}</p>
-      <span>Price:  ${data.price} USD</span>
-    </div>
-  </div>
-  `;
-}
-
-getData().then(loadData);
-console.log(getData()); 
-
-
-// $observe.addEventListener('click', () => {
-//     axios.get(API)
-//         .then(response => {
-//             console.log(response.data);
-//    
-
-// const intersectionObserver = new IntersectionObserver(entries => {
-//   // logic...
-// }, {
-//   rootMargin: '0px 0px 100% 0px',
-//   threshold: 0.5
-// });
-
-// intersectionObserver.observe($observe);
+getData();
