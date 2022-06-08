0632364f85bca67495fbee2949adac247a50ad1f
Carlos J. Ramirez <cramirez@jornaya.com>
2022-06-08 00:20:07 -0500

    Resolucion 1er, 2do y parte del 3er problema(s)

--- src/index.js
+++ src/index.js
@@ -1,29 +1,74 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const itemsPerPage = 10;
+const initialItem = 5;
+const debug = true;
 
-const getData = api => {
+const getData = (api, currentPage) => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
+      if(debug) console.log('current page = ' + currentPage);
+      let initialId = ((currentPage * itemsPerPage) - itemsPerPage + initialItem);
+      let endingId = ((currentPage * itemsPerPage) + initialItem - 1);
+      if(debug) console.log('initialId = ' + initialId + ', endingId = ' + endingId);
+      let itemsInPage = 0;
+      // let output = products.map(product => {
+      let output = products
+        .filter(product => (product.id >= initialId && product.id <= endingId))
+        .map(function (product) {
         // template
+        itemsInPage++;
+        return '' +
+          '<article class="Card">' +
+            '<h2>' + 
+            product.id +
+            ' | ' +
+            product.title + 
+            '<small>Precio: $' + product.price + '</small>' +
+            '</h2>' +
+            '<img src="' + product.images[0] +'"></img>' +
+            '<h2>' + product.description + '</h2>' +
+          '</article>';
       });
+      localStorage.setItem('itemsInPage', itemsInPage);
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
+      // if(debug) console.log(output);
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = (currentPage) => {
+  getData(API, currentPage);
+}
+
+window.onbeforeunload = function() {
+  localStorage.removeItem('pagination');
+  localStorage.removeItem('itemsInPage');
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  if(debug) console.log('..........');
+  if(localStorage.getItem('pagination') == null) {
+    localStorage.setItem('pagination', 1);
+    if(debug) console.log('initial page = ' + localStorage.getItem('pagination'));
+  } else {
+    let nextPage = parseInt(localStorage.getItem('pagination')) + 1;
+    if(debug) console.log('nextPage = ' + nextPage);
+    localStorage.setItem('pagination', nextPage);
+  }
+  loadData(parseInt(localStorage.getItem('pagination')));
+  if(debug) console.log('itemsInPage = ' + localStorage.getItem('itemsInPage'));
+  if(parseInt(localStorage.getItem('itemsInPage')) < itemsPerPage) {
+    // EOF
+    if(debug) console.log('Todos los productos Obtenidos'); // TODO
+  }
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
