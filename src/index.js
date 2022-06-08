a005b23f2ac7d3cbc1028276dbd8b078112ced0a
Christian Mora <christmo99@gmail.com>
2022-06-08 10:51:23 -0500

    fix: pasar las pruebas de cypress

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 let pagination = 5;
 let limit = 10;
+let firstInteraction = true;
 const API = 'https://api.escuelajs.co/api/v1/products?limit='+limit+'&offset=';
 
 const init = () =>{
@@ -9,46 +10,53 @@ const init = () =>{
 }
 
 const getData = async api => {
-    console.log(api);
-  await fetch(api)
+    await fetch(api)
     .then(response => response.json())
     .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-        return "<article class='Card'>" +
-            "<img src='"+product.images[parseInt(Math.random()*3)]+"' />" +
-            "<h2>" +
-            product.title +
-            "<small>$"+ product.price+"</small>" +
-            "</h2>"+
-        "</article>";
-      });
-      output.forEach(element => {
+        let products = response;
+        let output = products.map(product => {
+            // template
+            return "<article class='Card'>" +
+                "<img src='"+product.images[parseInt(Math.random()*3)]+"' />" +
+                "<h2>" +
+                product.id + " " + product.title +
+                "<small>$"+ product.price+"</small>" +
+                "</h2>"+
+            "</article>";
+        });
+        let items = document.createElement('section');
+        items.classList.add('Items');
+        output.forEach(element => {
           let newItem = document.createElement('section');
           newItem.classList.add('Item');
           newItem.innerHTML = element;
-          $app.appendChild(newItem);
-      });
+          items.appendChild(newItem);
+        });
+        $app.appendChild(items);
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-    getData(API+pagination);
-}
-
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
-  pagination = parseInt(localStorage.getItem("pagination")) + limit;
-  if(pagination <= 200){
-      localStorage.setItem("pagination", pagination);
-      getData(API+pagination);
-  } else {
-    const $notification = document.getElementById('notification');
-    $notification.innerHTML="<h1>Todos los productos Obtenidos</h1>";
-    intersectionObserver.disconnect();
-  }
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            if(pagination <= 200){
+                console.log("invocar:" + pagination);
+                getData(API+pagination);
+                if(firstInteraction){
+                    firstInteraction = false;
+                }else{
+                    pagination = parseInt(localStorage.getItem("pagination")) + limit;
+                    localStorage.setItem("pagination", pagination);
+                }
+            } else {
+                const $notification = document.getElementById('notification');
+                $notification.innerHTML="<h1>Todos los productos Obtenidos</h1>";
+                intersectionObserver.disconnect();
+            }
+        }
+    });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
