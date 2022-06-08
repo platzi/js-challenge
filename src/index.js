6011c25e1320ccb3faac6f8741a826de00aff6f3
Link <joel-e11@hotmail.com>
2022-06-08 10:45:40 -0500

    Correcciones realizadas

--- src/index.js
+++ src/index.js
@@ -4,38 +4,33 @@ const API = 'https://api.escuelajs.co/api/v1/products';
 
 localStorage.clear();
 
-const getData = async ({api, offset, limit}) => {
+const getData = async ({ api, offset, limit }) => {
   const url = `${api}?offset=${offset}&limit=${limit}`;
   localStorage.setItem('pagination', offset);
-  fetch(url)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map((product) => `
-        <article class="Card">
-          <img src="${product.images[0]}" />
-          <h2>
-            ${product.title}
-            <small>${product.price}</small>
-          </h2> 
-        </article>
-      `
-      );
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
 
-      if (products.length < limit) {
-        const message = document.createElement('p');
-        message.innerHTML = 'Todos los productos Obtenidos';
-        $app.appendChild(message);
-        intersectionObserver.unobserve($observe);
-        intersectionObserver.disconnect();
-      }
-      
-    })
-    .catch(error => console.log(error));
+  let data = await fetch(url);
+  let products = await data.json();
+  let output = products.map((product) => 
+    `<article class="Card">
+      <img src="${product.images[0]}" />
+      <h2>${product.title}
+        <small>${product.price}</small>
+      </h2> 
+    </article>`
+  );
+  let newItem = document.createElement('section');
+  newItem.classList.add('Item');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
+
+  if (products.length < limit) {
+    const message = document.createElement('p');
+    message.innerHTML = 'Todos los productos Obtenidos';
+    $app.appendChild(message);
+    intersectionObserver.unobserve($observe);
+    //intersectionObserver.disconnect();
+  }
+
 }
 
 const loadData = async (offset) => {
