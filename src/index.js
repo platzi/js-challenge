79f5bb67085953f7ac9beb2d5b1c694f966a8004
matisyk <matisyk@hotmail.com>
2022-06-08 00:34:00 -0300

    PROGRESS

--- src/index.js
+++ src/index.js
@@ -1,31 +1,56 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API = `https://api.escuelajs.co/api/v1/products`;
+
+localStorage.removeItem('pagination');
+localStorage.setItem('pagination', 5);
 
 const getData = api => {
-  fetch(api)
+  fetch(api + `?offset=${localStorage.getItem('pagination')}&limit=10`)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
+      let output = products.map(product => `
+         <article class="Card">
+           <img src=${product.category.image} />
+           <h2>
+             ${product.title}
+             <small>$ ${product.price}</small>
+           </h2>
+         </article>
+         `);
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
+      newItem.classList.add('Items');
+       newItem.innerHTML = output.join(' ');
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  await getData(API);
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+let page = 0
+
+const intersectionObserver = new IntersectionObserver(entries => { 
+  if (entries[0].isIntersecting) {
+   page++
+    localStorage.setItem('page', page);
+
+    if (page > 1) {
+      localStorage.setItem('pagination', parseInt(localStorage.getItem('pagination')) + 10)
+    }
+
+    if (page > 20) {
+     alert("Todos los productos Obtenidos")
+     intersectionObserver.unobserve($observe);
+    }
+
+    loadData()
+  }
+  }, {
+    rootMargin: '0px 0px 100% 0px',
+  });
 
 intersectionObserver.observe($observe);
