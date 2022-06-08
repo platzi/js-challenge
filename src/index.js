67597618b55b928afb1ff4f60baaf8c68f5bba14
Jonathan Torres <exodiakah@gmail.com>
2022-06-07 23:24:08 -0400

    Finalizacion del desarrollo

--- src/index.js
+++ src/index.js
@@ -1,31 +1,64 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
+/* INICIALIZANDO LA API FAKE DE PLATZI */
 const API = 'https://api.escuelajs.co/api/v1/products';
+let start = 5;
+let limit = 10;
+let maxProducts = 200;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+const getData = async (api) => {
 
-const loadData = () => {
-  getData(API);
-}
+  const pagination = localStorage.getItem("pagination") ? parseInt(localStorage.getItem("pagination")) + limit : start;
+
+  if (pagination <= maxProducts) {
+
+    localStorage.setItem("pagination", pagination);
+
+    const response = await fetch(api + `/?limit=${limit}&offset=${pagination}`);
+    const products = await response.json();
+    const output = await products.map(product => `
+      <article class="Card">
+        <img src="${product.images[0]}" alt="${product.title}" />
+        <h2>
+          ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>
+    `);
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    output.map((products) => {
+      newItem.innerHTML += products;
+    });
+    $app.appendChild(newItem);
+  } else {
+    let newItem = document.createElement('div');
+    newItem.classList.add("end-list");
+    newItem.innerHTML = "Todos los productos Obtenidos";
+    $app.appendChild(newItem);
+    $observe.remove();
+  }
+
+
+
+}//end-getData
+
+const loadData = async () => {
+  await getData(API);
+}//end-loadData
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      loadData();
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
-});
+});//end-Observer
+
+window.addEventListener("beforeunload", function (e) {
+  this.localStorage.clear()
+}, false);
 
 intersectionObserver.observe($observe);
