518e09b0094aec7b2e11a6df7480857afa253025
Alpacorp <alejandro.palacios88@gmail.com>
2022-06-09 00:10:21 -0500

    change text and order funtion onload

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ let offset = 4;
 let limit = 10;
 const baseURL = `${API}?offset=${offset}&limit=${limit}`;
 
+window.onload = () => {
+  if (localStorage.getItem("offset")) {
+    localStorage.setItem("offset", 0);
+  }
+};
+
 const getData = (api) => {
   fetch(api)
     .then((response) => response.json())
@@ -22,34 +28,28 @@ const getData = (api) => {
             `;
       });
       let newItem = document.createElement("section");
-      let empty = document.createElement("h1");
+      let empty = document.createElement("h3");
 
       newItem.classList.add("Item");
       newItem.innerHTML = output;
-      empty.innerHTML = "No hay productos";
+      empty.innerHTML = "Todos los productos Obtenidos";
 
-      if (output.length > 0) {
-        $app.appendChild(newItem);
-      } else {
+      if (output.length === 0) {
         $app.appendChild(empty);
+      } else {
+        $app.appendChild(newItem);
       }
     })
     .catch((error) => console.log(error));
 };
 
-const loadData = (entries, observer) => {
+const loadData = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       getData(`${API}?offset=${(offset += 10)}&limit=${limit}`);
       localStorage.setItem("offset", offset);
     }
   });
-};
-
-window.onload = () => {
-  if (localStorage.getItem("offset")) {
-    localStorage.setItem("offset", 0);
-  }
 };
 
 const intersectionObserver = new IntersectionObserver(loadData, {
