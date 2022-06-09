9fe3c4682ba1df88647a168a95cc8b19f36e14fb
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-06-09 17:02:35 -0500

    soluciones

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = async api => {
-  const limit = 10;
-  let offset = 5;
-  const response = await fetch(`${api}?limit=${limit}&offset=${offset}`);
-  const data = await response.json();
-  console.log(data);
+const limit = 10;
+const offset_initial = 5;
+const pagination = 'pagination';
 
-  await fetch(response.url)
+const getData = api => {
+  return fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
-        return `
+        let article = `
           <article class="Card">
             <img src="${product.images[0]}" />
             <h2>
@@ -24,32 +21,59 @@ const getData = async api => {
             </h2>
           </article>
         `;
-      }).join('');
-            
-
-      if(products.length === 0){
-        intersectionObserver.unobserve($observe);
-        output = "<small>Todos los productos obtenidos</small>";
-      }
+        
+        return article;
+      }) .join('');
       
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      if(output.length !== 0) {
+        let newItem = document.createElement('section');
+        newItem.classList.add('Items');
+        newItem.innerHTML = output;
+        $app.appendChild(newItem);
+      } else {
+        stopObserver();
+      }
+
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async (offset, limit) => {
+  await getData(`${API}?offset=${offset}&limit=${limit}`);
+  console.log('Loading...' + limit);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
+  // logic...
   entries.forEach(entry => {
-    entry.intersectionRatio > 0 ? loadData() : null;
+    if(entry.isIntersecting) {
+      let offset = localStorage.getItem(pagination);
+      if(offset == null) {
+        offset = offset_initial;
+      } else {
+        offset = parseInt(offset) + limit;
+      }
+
+      loadData(offset, limit);
+
+      localStorage.setItem(pagination, offset);
+      }
   });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
+
+window.onbeforeunload = () => {
+  localStorage.removeItem(pagination);
+};
+
+const stopObserver = () => {
+  intersectionObserver.unobserve($observe);
+  intersectionObserver.disconnect();
+  let text = document.createElement('p');
+  text.textContent = 'Todos los productos Obtenidos';
+  $app.appendChild(text);
+  
+}
 
 intersectionObserver.observe($observe);
