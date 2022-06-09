5e6c034ccf3d82fe0548dfb8a843b98ce156103a
blackheartpearl <sebastianamaya958@gmail.com>
2022-06-09 14:03:00 -0500

    localStorage

--- src/index.js
+++ src/index.js
@@ -2,28 +2,46 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+const dOffset = 4;
+const dLimit = 10;
+localStorage.setItem('offset', dOffset);
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
       let output = products.map(product => {
-        // template
+        `<article class='Card'>
+          <img src='${product.images[0]}' />
+          <h2>              
+          ${product.title}
+          <small>$${product.price}-${product.id}</small>
+          </h2>
+          </article>`
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      if(products.length < dLimit){
+        let span = document.createElement('span');
+        span.classList.add('message');
+        span.innerHTML = 'Todos los productos obtenidos';
+        $app.appendChild(span);
+        intersectionObserver.disconnect()
+      }
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async (offset = 4, limit = dLimit) => {
+  localStorage.setItem('offset', `${offset+limit}`)
+  await getData(`${API}?offset=${offset}&limit=${limit}`);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
