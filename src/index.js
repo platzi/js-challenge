b926cc804b5e86bf400f70b4a56c60befd19032b
Hipólito <hmoralesjz@gmail.com>
2022-06-08 22:09:47 -0500

    Use simple pagination

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+const PAGINATION_KEY = 'pagination'; 
 
-const getData = api => {
-  fetch(api)
+const getData = (api, offset, limit) => {
+  const endpoint = `${api}?offset=${offset}&limit=${limit}`;
+
+  fetch(endpoint)
     .then(response => response.json())
     .then(response => {
       let products = response;
+      // By some reason, the products array starts at id 3
       let output = products.map(product => {
-        // template
+        return product.title + '<br>' ;
       });
+
+      localStorage.setItem(PAGINATION_KEY, offset + 10);
+
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
       newItem.innerHTML = output;
@@ -19,13 +26,28 @@ const getData = api => {
 }
 
 const loadData = () => {
-  getData(API);
+  let pagination = localStorage.getItem(PAGINATION_KEY);
+
+  let nPagination;
+  if (pagination) {
+    try { 
+      nPagination = Number(pagination);
+    } catch {
+      nPagination = 5;
+    }
+  } else {
+    nPagination = 5;
+  }
+
+  const limit = 10;
+  getData(API, nPagination, limit);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
+loadData();
