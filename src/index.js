194c6d8e663b180f9c1fd95e0ee99f98e9ea3bd0
SantiagoIvan <santiago.feijoo96@gmail.com>
2022-06-09 16:25:47 -0300

    Paginacion en local storage, callback del observer, se cargan los datos nuevos cuando se alcanza el final de pagina y se crean los nuevos objetos html dinamicamente

--- src/index.js
+++ src/index.js
@@ -1,31 +1,57 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
-
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+const $app = document.getElementById('app')
+const $observe = document.getElementById('observe')
+const API = 'https://api.escuelajs.co/api/v1/products'
+const limit = 10
+let offset = 5 // posicion inicial segun el enunciado
+localStorage.setItem('pagination', offset)
 
 const loadData = () => {
-  getData(API);
+    getData(API)
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const getData = (api) => {
+    const pag = localStorage.getItem('pagination')
+    fetch(`${api}?offset=${pag}&limit=${limit}`)
+        .then((response) => response.json())
+        .then((response) => {
+            let products = response
+            let output = products
+                .map((product) => {
+                    return createProduct(product)
+                })
+                .join('')
+            let newItem = document.createElement('section')
+            newItem.classList.add('Item')
+            newItem.innerHTML = output
+            $app.appendChild(newItem)
+        })
+        .catch((error) => console.log(error))
+    localStorage.setItem('pagination', offset + limit)
+    console.log('asd')
+}
 
-intersectionObserver.observe($observe);
+const createProduct = (productInfo) => {
+    return `
+    <article class="Card">
+      <img src='${productInfo.images[0]}' alt='${productInfo.title} img'/>
+      <h2>
+        ${productInfo.title}
+        <small>$${productInfo.price}</small>
+      </h2>
+    </article>
+    `
+}
+
+const onIntersect = ([entry]) => {
+    if (entry.isIntersecting) {
+        console.log('Loading data')
+        offset += limit
+        loadData()
+    }
+}
+
+const intersectionObserver = new IntersectionObserver(onIntersect, {
+    rootMargin: '0px 0px 100% 0px',
+})
+
+intersectionObserver.observe($observe)
