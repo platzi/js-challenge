bb3285ccc63274e2eb8e7e2250aaa3242097524f
distordev <adanuriplata@gmail.com>
2022-06-09 17:40:29 -0500

    Primer Problema

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
+let pagination = 4;
 
-const getData = api => {
+if  (localStorage.getItem('pagination')) {
+  pagination = localStorage.getItem('pagination')
+} else {
+  localStorage.setItem('pagination', pagination);
+}
+const getData = (api, initial) => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
-        // template
+      if(parseInt(localStorage.getItem('pagination')) >= products.length) {
+        initial = 4;
+        localStorage.setItem('pagination', initial);
+      }
+      let output = products.slice(initial, initial+10 ).map(product => {
+        return `<h1>${product.title}</h1> <br/> <p>${product.id}</p><br/> <p>${product.description} </p>`
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -18,12 +28,14 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = (initial) => {
+  getData(API, initial );
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  pagination = localStorage.getItem('pagination');
+  loadData(parseInt(pagination));
+  localStorage.setItem('pagination', parseInt(pagination) + 10);
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
