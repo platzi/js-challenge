5e4502be2931d259eadf633c01494f703285af7f
distordev <adanuriplata@gmail.com>
2022-06-09 18:22:42 -0500

    Tercer Problema

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
-let pagination = 4;
+let pagination = 0;
 
-if  (localStorage.getItem('pagination')) {
-  pagination = localStorage.getItem('pagination')
-} else {
-  localStorage.setItem('pagination', pagination);
-}
-const getData = (api, initial) => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      if(parseInt(localStorage.getItem('pagination')) >= products.length) {
-        initial = 4;
-        localStorage.setItem('pagination', initial);
-      }
-      let output = products.slice(initial, initial+10 ).map( product => {
+const getData = async (api, initial) => {
+  const response = await fetch(api)
+  const responseJson = await response.json()
+  const products = responseJson
+    let output = products.slice(initial, initial+10 ).map( product => {
         return `<article class="Card">
         <img src="${product.images[0]}" alt='${product.title}' />
         <h2>
@@ -30,18 +20,15 @@ const getData = (api, initial) => {
       newItem.classList.add('Item');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+    }
 
 const loadData = (initial) => {
   getData(API, initial );
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  pagination = localStorage.getItem('pagination');
   loadData(parseInt(pagination));
-  localStorage.setItem('pagination', parseInt(pagination) + 10);
+  pagination = pagination + 10
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
