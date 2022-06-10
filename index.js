3d8f8777eda213b476baff29839dd219ea37e4dc
sergio valadez <sergioval@gmail.com>
2022-06-09 23:50:25 -0500

    Fix double rendering

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products?offset=6&limit=10';
+const API = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=5';
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
+      products.map(product => {
         product.price = product.price.toLocaleString('en-EU', { style: 'currency', currency: 'USD' });
-        return `
+        let newItem = document.createElement('div');
+        newItem.classList.add('Item');
+        newItem.innerHTML = `
         <article class="Card">
-        <img src="${product.category.image}" />
+        <img src="${product.images[0]}" />
         <h2>
-          ${product.title}
-          <small>${product.price}</small>
+        ${product.title}
+        <small>${product.price}</small>
         </h2>
         <p>${product.description}</p>
-      </article>
-        `;	
+        </article>`;	
+        $app.appendChild(newItem);
       });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
 
-const loadData = () => {
+const loadData = async () => {
   getData(API);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  entries.push(entries[0]);
-  entries.forEach(entry => {
+  entries.map(entry => {
     if (entry.isIntersecting) {
+      console.log(typeof(entry.target));
+      getData(API);
       loadData();
     }
   });
 }, {
   rootMargin: '0px 0px 100% 0px',
+  threshold: 0.5
 });
 
 intersectionObserver.observe($observe);
