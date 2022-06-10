7dd93aa6ad5d07454acee90e5dec4ef5eab24d53
fedelmar <fsuarez91@gmail.com>
2022-06-10 20:22:46 -0300

    improve code

--- src/index.js
+++ src/index.js
@@ -8,56 +8,62 @@ const getData = (api) => {
     .then((response) => {
       let products = response;
 
-      let output = products.map((product) => {
-        let price = document.createElement("small");
-        let title = document.createElement("h2");
-        let img = document.createElement("img");
-        let article = document.createElement("article");
-        article.classList.add("Card");
-
-        price.innerText = `$ ${product.price}`;
-        title.innerText = `${product.title} id: ${product.id} `;
-        title.appendChild(price);
-        img.setAttribute("src", product.images[0]);
-
-        article.appendChild(img);
-        article.appendChild(title);
-
-        console.log(product)
-        return article.outerHTML;
-      });
+      let output = products.map(
+        (product) =>
+          `<article class="Card">
+            <img src=${product.images[2]}/>
+            <h2> ${product.title} <small>$ ${product.price}</small> </h2>
+        </article>`
+      );
 
       let newItem = document.createElement("section");
       newItem.classList.add("Items");
       newItem.innerHTML = output.join("");
       $app.appendChild(newItem);
+
+      if (products.length < 10) {
+        let message = document.createElement('p');
+        message.classList.add('Message');
+        message.innerText = 'Todos los productos Obtenidos';
+        $app.appendChild(message);
+        $observe.remove();
+    }
     })
     .catch((error) => console.log(error));
 };
 
 const loadData = async (offset, limit) => {
-  const page = parseInt(localStorage.pagination);
-  offset = offset * page;
   await getData(`${API}?offset=${offset}&limit=${limit}`);
-  localStorage.setItem("pagination", page + 1);
 };
 
 const options = {
-  root: null,
   rootMargin: "0px 0px 100% 0px",
-  threshold: 1.0,
 };
 
-const offset = 10;
 const limit = 10;
+const maxProducts = 200;
 localStorage.setItem("pagination", 0);
 const intersectionObserver = new IntersectionObserver((entries) => {
   // logic...
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
+      let offset = parseInt(localStorage.pagination);
+
+      if (offset == null) {
+        offset = 5;
+      } else {
+        offset = parseInt(offset) + limit;
+      }
+
       loadData(offset, limit);
+
+      localStorage.setItem('pagination', offset);
     }
   });
 }, options);
+
+window.addEventListener("beforeunload", () => {
+  localStorage.removeItem("pagination");
+});
 
 intersectionObserver.observe($observe);
