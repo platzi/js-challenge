e7e2447226547fdca6df3f0d9b90d0f7023a7b82
sergio valadez <sergioval@gmail.com>
2022-06-10 17:40:53 -0500

    Set offset and limit variables

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products?offset=0&limit=5';
+let offset= 0;
+let limit= 10;
+const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      products.map(product => {
-        product.price = product.price.toLocaleString('en-EU', { style: 'currency', currency: 'USD' });
-        let newItem = document.createElement('div');
-        newItem.classList.add('Item');
-        newItem.innerHTML = `
-        <article class="Card">
-        <img src="${product.images[0]}" />
-        <h2>
-        ${product.title}
-        <small>${product.price}</small>
-        </h2>
-        <p>${product.description}</p>
-        </article>`;	
-        $app.appendChild(newItem);
-      });
-    })
-    .catch(error => console.log(error));
+
+const getData = API => {
+  fetch(API+`?offset=${offset}&limit=${limit}`)
+  .then(response => response.json())
+  .then(response => {
+    localStorage.setItem('pagination', limit);
+    console.log(response.length);
+    let products = response;
+    products.map(product => {
+      product.price = product.price.toLocaleString('en-EU', { style: 'currency', currency: 'USD' });
+      let newItem = document.createElement('div');
+      newItem.classList.add('Item');
+      newItem.innerHTML = `
+      <article class="Card">
+      <img src="${product.images[0]}" />
+      <h2>
+      ${product.title}
+      <small>${product.price}</small>
+      </h2>
+      <p>${product.description}</p>
+      </article>`;	
+      $app.appendChild(newItem);
+    });
+  })
+  .catch(error => console.log(error));
 }
+console.log(offset)
+offset += 10;
 
 
 const loadData = async () => {
   getData(API);
 }
 
-const intersectionObserver = new IntersectionObserver(entries => {
+const options = {
+  root: null,
+  threshold: 0,
+  rootMargin: '0px 0px 100% 0px'
+}
+
+
+
+const Observer = new IntersectionObserver(entries => {
   entries.map(entry => {
+    console.log(entry.target);
     if (entry.isIntersecting) {
       console.log(typeof(entry.target));
       getData(API);
       loadData();
     }
   });
-}, {
-  rootMargin: '0px 0px 100% 0px',
-  threshold: 0.5
-});
+}, 
+options);
 
-intersectionObserver.observe($observe);
+Observer.observe($observe);
