566d98642281e4637e991984731cef8c5398f278
Edwin Cuba Davila <edwincubad@gmail.com>
2022-06-11 19:58:46 -0500

    [DONE] challenge complete

--- src/index.js
+++ src/index.js
@@ -2,30 +2,65 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      let products = response;
-      let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+localStorage.clear();
 
-const loadData = () => {
-  getData(API);
-}
+const PAGINTAION_STORAGE = 'pagination';
+const initialPagination = 5;
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const getData = async (api) => {
+  const response = await fetch(api);
+  const products = await response.json();
+
+  if (!products.length) {
+    intersectionObserver.unobserve($observe);
+    const message = document.createElement('p');
+    message.innerHTML = 'Todos los productos Obtenidos';
+    return $app.appendChild(message);
+  }
+
+  const output = products.map(
+    (product) => `
+      <article class="Card">
+        <img src="${product.images[0]}" />
+        <h2>
+          ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>
+    `,
+  );
+
+  const newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output.join('');
+  $app.appendChild(newItem);
+};
+
+const getNewPagination = () => {
+  const pagination = localStorage.getItem(PAGINTAION_STORAGE);
+  localStorage.setItem(PAGINTAION_STORAGE, pagination ? Number(pagination) + 10 : initialPagination);
+  return localStorage.getItem(PAGINTAION_STORAGE);
+};
+
+const loadData = async () => {
+  const pagination = getNewPagination();
+  const objPAarams = { offset: pagination, limit: 10 };
+  const strParams = new URLSearchParams(objPAarams).toString();
+  const PARAMETERIZED_API = `${API}?${strParams}`;
+  await getData(PARAMETERIZED_API);
+};
+
+let = false;
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: '0px 0px 100% 0px',
+    threshold: 1,
+  },
+);
 
 intersectionObserver.observe($observe);
