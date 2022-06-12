ae78bfa798250f7bcc4f77551003b6d64043e1b8
masterarias <masterarias@users.noreply.github.com>
2022-06-12 08:44:11 +0200

    logging / debug

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ const getData = async api => {
   newItem.innerHTML = output;
   $app.appendChild(newItem);
   localStorage.setItem('pagination', current + 10);
+  console.log(localStorage.getItem('pagination')); 
 }
 
 const loadData = () => {
