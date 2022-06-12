4ba780c9f8cfd17abf2f6a3973c77c8f99651bed
nilt0n <info@nilton.org>
2022-06-11 22:51:29 -0500

    js Challenge  webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 // webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path');
 module.exports = {
     entry: './src/index.js',
@@ -7,6 +8,11 @@ module.exports = {
       filename: 'bundle.js',
     },
     devtool: 'eval-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'public/index.html'
+        })
+      ]
   };
   
   
