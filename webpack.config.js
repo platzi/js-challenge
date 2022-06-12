fa2d263fab7810fd19b551d2086f96e766fe9495
Andrea Vellicce <41125824+AnDyLaTuquita@users.noreply.github.com>
2022-06-12 17:16:34 -0500

    Configuración para Deploy

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/public')
+    },
+    port: 9000,
+  }
+}
