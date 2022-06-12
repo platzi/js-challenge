d051e0b06e3f021eef8e41ff9013154a5979127f
nilt0n <info@nilton.org>
2022-06-12 00:15:06 -0500

    js Challenge  webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
-    entry: 'src/index.js',
+    entry: './src/index.js',
     output: {
       path: path.resolve(__dirname + './dist'),
       filename: 'bundle.js',
