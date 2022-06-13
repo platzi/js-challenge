adff29cf9fd82531035075f56b0f37816eb61173
Andrea Vellicce <41125824+AnDyLaTuquita@users.noreply.github.com>
2022-06-12 19:48:25 -0500

    Configuración para Deploy

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
