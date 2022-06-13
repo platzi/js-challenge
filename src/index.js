276426549ae47f6aa7ed64bb104f56838e2d629f
Brandon López <bralf_project-01@outlook.es>
2022-06-12 19:28:48 -0500

    Cambios: Solventando problema 1 (2,3,4). Realizando problema 3.(1,2,3). Realizando problema 4 (1,2). Corrección de pagination.

--- src/index.js
+++ src/index.js
@@ -4,28 +4,47 @@ const API = 'https://api.escuelajs.co/api/v1/products';
 const PRODUCT_OFFSET = 5;
 const PRODUCT_LIMIT = 10;
 
+const setPagination = (value) => {
+  localStorage.setItem("pagination", value);
+}
+
+const getPagination = () => {
+  if (!localStorage.getItem("pagination")) {
+    localStorage.setItem("pagination", PRODUCT_OFFSET);;
+  }
+
+  return parseInt(localStorage.getItem("pagination"));
+}
+
+const clearPagination = () => {
+  localStorage.removeItem("pagination");
+}
+
 const getData = async(api) => {
+  let offset = getPagination();
+  if ($app.childNodes.length !== 0) {
+    setPagination(offset + PRODUCT_LIMIT);
+  }
+  
   try{
-    const response = await fetch(API);
+    const response = await fetch(`${api}?offset=${offset}&limit=${PRODUCT_LIMIT}`);
     if(response.status != 200){ 
-      throw new error(response.message);
+      console.log("Algo salió mal !");
     }
     const data = await response.json();
-    //console.log(data);
+    
     return data;
-  }catch (err)
+  }catch (ex)
   {
-    throw new error(err);
-    console.log("Error: " + err);
+    console.log("Error: " + ex);
   }
 }
 
 const loadData = async() => {
-  // getData(API);
   try{
-    let products = await getData(`${API}?offset=${PRODUCT_OFFSET}&limit=${PRODUCT_LIMIT}`)
+    let products = await getData(API);
     let structure = ``;
-
+    
     products.forEach(product => {
       structure += `
           <article id="product_${product.id}" class="Card">
@@ -39,23 +58,32 @@ const loadData = async() => {
     });
 
     let catalogue = document.createElement("section");
-    catalogue.classList.add("Items"); //
+    catalogue.classList.add("Items");
     catalogue.innerHTML = structure;
     $app.appendChild(catalogue);
 
+    if (products.length < PRODUCT_LIMIT) {
+      let span = document.createElement("span");
+      span.classList.add("Message");
+      span.innerHTML = "Todos los productos obtenidos";
+      $app.appendChild(span);
+      intersectionObserver.disconnect();
+    }
   }
-  catch(err){
-    // throw new error(err);
-    console.error(err);
+  catch(ex){
+    console.log(ex);
   }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadData();
+    }
+  });
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
-//getData();
-loadData();
+clearPagination();
