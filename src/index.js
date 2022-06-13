d018e5186001eed8b83144362424bb7bc81933a4
gitguti <gitmelgtrrz1@gmail.com>
2022-06-13 19:31:35 -0400

    get & set localStorage pagination

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+const limitPaginate = 10;
+let offsetPaginate=5;
+
+if (localStorage.getItem("pagination")) {
+  //parseo el localStorage que es un string para sumar
+  offsetPaginate = parseInt(localStorage.getItem("pagination")) + limitPaginate;
+
+  //set nuevo offsetPaginate del localStorage
+  localStorage.setItem("pagination", offsetPaginate);
+  console.log("offsetPaginate",offsetPaginate);
+} else {
+  //si no existe, entonces se toma el valor definido en offsetPaginate
+  localStorage.setItem("pagination", offsetPaginate);
+}
+
+let urlPaginate=`?offset=` + offsetPaginate + `&limit=` + limitPaginate;
+
+const loadData = (papa) => {
+
+  console.log(API+urlPaginate);
+  getData(API+urlPaginate);
+  console.log("hola", papa);
+}
+
+window.onload = loadData;
+  
 const getData = api => {
   fetch(api)
     .then(response => response.json())
@@ -18,8 +44,22 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const traerMas = () => {
+  console.log(typeof(urlPaginate))
+  console.log(urlPaginate)
+  console.log(typeof(limitPaginate))
+
+    //parseo el localStorage que es un string para sumar
+    offsetPaginate = parseInt(localStorage.getItem("pagination")) + limitPaginate;
+
+    //set nuevo offsetPaginate del localStorage
+    localStorage.setItem("pagination", offsetPaginate);
+    console.log("offsetPaginate",offsetPaginate);
+ 
+  console.log(localStorage.getItem("pagination"));
+  //nuevamente se construye la url para que vaya incrementado
+  urlPaginate =`?offset=` + localStorage.getItem("pagination") + `&limit=` + limitPaginate;
+  console.log(urlPaginate); 
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
@@ -29,3 +69,4 @@ const intersectionObserver = new IntersectionObserver(entries => {
 });
 
 intersectionObserver.observe($observe);
+
