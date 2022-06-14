8c60661d41e53da628fe482346db128c5e86b373
aguscassi <m.agustina.cassi@gmail.com>
2022-06-14 16:52:39 -0300

    Challenge Solved

--- src/index.js
+++ src/index.js
@@ -1,31 +1,65 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://api.escuelajs.co/api/v1/products";
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
+const limit = 10;
+
+window.addEventListener("beforeunload", ()=> {
+  localStorage.removeItem("pagination")
+})
+
+const getData = (api, offset, limit) => {
+  const endpoint = `${API}?offset=${offset}&limit=${limit}`;
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((response) => {
       let products = response;
+      if(products.length > 0){
       let output = products.map(product => {
-        // template
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
+        return `<article class="Card">
+        <img class="Card-img" src=${product.images[0]} alt=${product.title}/>
+        <h2 class="Card-h2">
+          ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>`;
+      }); 
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items"); 
+      newItem.innerHTML = output.join("");
       $app.appendChild(newItem);
+    }else{
+      let finalMessage = document.createElement("p")
+      finalMessage.innerText = "Todos los productos obtenidos 📦"
+      $app.appendChild(finalMessage)
+      intersectionObserver.unobserve($observe)
+    }
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.error(error));
+};
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = async () => {
+  if(!localStorage.getItem("pagination")){
+    let pagination = await localStorage.setItem("pagination", 5)
+    let obtainData = await getData(API, localStorage.getItem("pagination"), limit)
+  }else{
+    let offset = await parseInt(localStorage.getItem("pagination")) + limit;
+    let changeStorage = await localStorage.setItem("pagination", offset)
+    let obtainData = await getData(API, localStorage.getItem("pagination"), limit)
+  }
+};
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if(entry.intersectionRatio > 0){
+        loadData()
+      } 
+    })
+  },
+  {
+    rootMargin: "0px 0px 100% 0px",
+  }
+);
 
 intersectionObserver.observe($observe);
